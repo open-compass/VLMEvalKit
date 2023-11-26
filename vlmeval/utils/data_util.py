f96200b8ea51d8e0e0bd743a3d19f03ff135bcd2
kennymckormick <dhd@pku.edu.cn>
2023-11-26 20:29:08 +0800

    update

--- vlmeval/utils/data_util.py
+++ vlmeval/utils/data_util.py
@@ -16,6 +16,8 @@ class TSVDataset:
             'MMBench_TEST_EN': "https://opencompass.openxlab.space/utils/VLMEval/MMBench_TEST_EN.tsv", 
             'MMBench_DEV_CN': "https://opencompass.openxlab.space/utils/VLMEval/MMBench_DEV_CN.tsv", 
             'MMBench_TEST_CN': "https://opencompass.openxlab.space/utils/VLMEval/MMBench_TEST_CN.tsv", 
+            "MMBench": "https://opencompass.openxlab.space/utils/VLMEval/MMBench.tsv",  # Link Invalid, Internal Only
+            "MMBench_CN": "https://opencompass.openxlab.space/utils/VLMEval/MMBench_CN.tsv",    # Link Invalid, Internal Only
             'CCBench': "https://opencompass.openxlab.space/utils/VLMEval/CCBench.tsv", 
             'MME': "https://opencompass.openxlab.space/utils/VLMEval/MME.tsv", 
             'SEEDBench_IMG': "https://opencompass.openxlab.space/utils/VLMEval/SEEDBench_IMG.tsv", 
