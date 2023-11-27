f3231f0a0d04cec8335408776b08dde2aa550144
kennymckormick <dhd@pku.edu.cn>
2023-11-27 11:09:02 +0800

    update

--- vlmeval/vlm/minigpt4.py
+++ vlmeval/vlm/minigpt4.py
@@ -3,6 +3,7 @@ import sys
 from abc import abstractproperty
 import os.path as osp
 from transformers import StoppingCriteriaList
+from PIL import Image
 
 class MiniGPT4:
 
@@ -47,26 +48,25 @@ class MiniGPT4:
         vis_processor_cfg = cfg.datasets_cfg.cc_sbu_align.vis_processor.train
         vis_processor = registry.get_processor_class(vis_processor_cfg.name).from_config(vis_processor_cfg)
         self.model = model
-        self.vis_processor = vis_processor
-        self.CONV_VISION = CONV_VISION_LLama2 if self.mode == 'v2' else CONV_VISION_Vicuna0
-
-        stop_words_ids = [[835], [2277, 29937]]
-        stop_words_ids = [torch.tensor(ids).to(device) for ids in stop_words_ids]
-        self.stopping_criteria = StoppingCriteriaList([StoppingCriteriaSub(stops=stop_words_ids)])
+        self.vis_processor = vis_processor  
 
         
     def generate(self, image_path, prompt):
-        from minigpt4.conversation.conversation import Chat
-        if self.mode == 'v2':
-            chat = Chat(self.model, self.vis_processor, device=self.device)
-        else:
-            chat = Chat(self.model, self.vis_processor, device=self.device, stopping_criteria=self.stopping_criteria)
-
-        chat_state = self.CONV_VISION.copy()
-        img_list = []
-        _ = chat.upload_img(image_path, chat_state, img_list)
-        chat.encode_img(img_list)
-        chat.ask(prompt, chat_state)
-        msg = chat.answer(conv=chat_state, img_list=img_list, temperature=0.01, 
-                          max_new_tokens=500, max_length=2000)[0]
-        return msg
+        raw_image = Image.open(image_path).convert('RGB')
+        images = self.vis_processor(raw_image).unsqueeze(0)
+        img_prompt = '###Human: <Img><ImageHere></Img> '
+        prompt = img_prompt + ' ' + prompt
+        texts = [prompt]
+        outputs = self.model.generate(
+            images,
+            texts,
+            num_beams=1,
+            max_new_tokens=20,
+            min_length=1,
+            top_p=0.9,
+            repetition_penalty=1,
+            length_penalty=1,
+            temperature=1,
+            do_sample=False,
+            stop_words_ids=[2])
+        return outputs[0]
