980703d4cddb598e841367539bf9d8bb9bcf233f
kennymckormick <dhd@pku.edu.cn>
2023-11-27 11:14:50 +0800

    update

--- vlmeval/vlm/minigpt4.py
+++ vlmeval/vlm/minigpt4.py
@@ -27,11 +27,10 @@ class MiniGPT4:
         self.temperature = temperature 
         self.max_out_len = max_out_len
         self.root = root 
-        self.cfg = osp.join(root, cfg)
+        self.cfg = osp.join('misc', cfg)
         sys.path.append(self.root)
         from minigpt4.common.config import Config
         from minigpt4.common.registry import registry
-        from minigpt4.conversation.conversation import StoppingCriteriaSub, CONV_VISION_Vicuna0, CONV_VISION_LLama2
 
         device = torch.cuda.current_device()
         self.device = device
