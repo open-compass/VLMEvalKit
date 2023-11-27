15c8dcb285f4947bf80432887c427c911119d369
kennymckormick <dhd@pku.edu.cn>
2023-11-27 19:55:04 +0800

    update

--- vlmeval/vlm/visualglm.py
+++ vlmeval/vlm/visualglm.py
@@ -14,6 +14,7 @@ class VisualGLM:
             warnings.warn("Please install SwissArmyTransformer to use VisualGLM")
         self.model_paths = [
             '/mnt/lustre/duanhaodong/petrel_share/visualglm-6b',
+            '/cpfs01/shared/llmeval/dhd/visualglm-6b',
             "THUDM/visualglm-6b"
         ]
         model_path = None
