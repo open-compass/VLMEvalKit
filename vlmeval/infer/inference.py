3e2a6d3e8753b21a5c4e3d703dc57c62cb9d347f
kennymckormick <dhd@pku.edu.cn>
2023-12-19 22:06:24 +0800

    [Feature] Support API model in inference.py

--- vlmeval/infer/inference.py
+++ vlmeval/infer/inference.py
@@ -2,7 +2,7 @@ import torch
 import torch.distributed as dist
 import datetime
 from vlmeval.config import supported_VLM
-from vlmeval.utils import TSVDataset
+from vlmeval.utils import TSVDataset, track_progress_rich
 from vlmeval.eval import MME_rating, MME_postproc
 from vlmeval.smp import *
 
@@ -14,7 +14,7 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-def infer_data(model_name, dataset_name, out_file, verbose=False):
+def infer_data(model_name, dataset_name, out_file, verbose=False, api_nproc=4):
     res = {}
     if osp.exists(out_file):
         res = load(out_file)
@@ -43,6 +43,31 @@ def infer_data(model_name, dataset_name, out_file, verbose=False):
         model = supported_VLM[model_name]()
     else:
         model = model_name
+
+    is_api = getattr(model, 'is_api', False)
+    if is_api:
+        assert world_size == 1
+        data = dataset.data
+        lt, indices = len(data), data['index']
+        structs = [dataset.build_prompt(data.iloc[i: i + 1]) for i in range(lt)]
+        
+        if dataset_name in ['CORE_MM']:
+            assert hasattr(model, 'multi_generate')
+            structs = [dict(image_paths=struct['image'], prompt=struct['text'], dataset=dataset_name) for struct in structs]
+        else:
+            structs = [dict(image_path=struct['image'], prompt=struct['text'], dataset=dataset_name) for struct in structs]
+        res = track_progress_rich(
+            model.multi_generate if dataset_name in ['CORE_MM'] else model.generate, 
+            structs, 
+            nproc=api_nproc, 
+            chunksize=api_nproc, 
+            save=out_file,
+            keys=indices)
+        result = load(out_file)
+        for idx, text in zip(indices, res):
+            if idx in result:
+                assert result[idx] == text 
+        return model
 
     for i in tqdm(range(lt)):
         idx = data.iloc[i]['index']
