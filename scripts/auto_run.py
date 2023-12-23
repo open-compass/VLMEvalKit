e2fea3c1ef6ffaa2eb5d72b6479742da1a75584e
kennymckormick <dhd@pku.edu.cn>
2023-12-23 23:29:55 +0800

    update auto_run

--- scripts/auto_run.py
+++ scripts/auto_run.py
@@ -4,15 +4,16 @@ from vlmeval.config import supported_VLM
 def is_api(x):
     return getattr(supported_VLM[x].func, 'is_api', False)
 
-DATASET = 'OCRVQA_TESTCORE'
+datasets = ['OCRVQA_TESTCORE', 'TextVQA_VAL']
 models = list(supported_VLM)
 models = [x for x in models if 'fs' not in x]
 models = [x for x in models if not is_api(x)]
 
-for m in models:
-    if '80b' in m:
-        cmd = f'python run.py --data {DATASET} --model {m} --verbose'
-    else:
-        cmd = f'torchrun --nproc-per-node=8 run.py --data {DATASET} --model {m} --verbose'
-    print(cmd)
-    os.system(cmd)
+for DATASET in datasets:
+    for m in models:
+        if '80b' in m:
+            cmd = f'python run.py --data {DATASET} --model {m}'
+        else:
+            cmd = f'torchrun --nproc-per-node=8 run.py --data {DATASET} --model {m}'
+        print(cmd)
+        os.system(cmd)
