ca1e0eca3a21c3b23ee5a3f772f7f4877eb73789
kennymckormick <dhd@pku.edu.cn>
2023-12-24 14:35:47 +0800

    update

--- scripts/auto_run.py
+++ scripts/auto_run.py
@@ -5,15 +5,15 @@ def is_api(x):
     return getattr(supported_VLM[x].func, 'is_api', False)
 
 datasets = ['OCRVQA_TESTCORE', 'TextVQA_VAL']
+dataset_str = ' '.join(datasets)
 models = list(supported_VLM)
 models = [x for x in models if 'fs' not in x]
 models = [x for x in models if not is_api(x)]
 
-for DATASET in datasets:
-    for m in models:
-        if '80b' in m:
-            cmd = f'python run.py --data {DATASET} --model {m}'
-        else:
-            cmd = f'torchrun --nproc-per-node=8 run.py --data {DATASET} --model {m}'
-        print(cmd)
-        os.system(cmd)
+for m in models:
+    if '80b' in m:
+        cmd = f'python run.py --data {dataset_str} --model {m}'
+    else:
+        cmd = f'torchrun --nproc-per-node=8 run.py --data {dataset_str} --model {m}'
+    print(cmd)
+    os.system(cmd)
