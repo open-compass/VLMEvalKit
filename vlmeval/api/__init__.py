d97883ddefc2417589c1da298c1298c3f3033b49
“llllIlllll” <1246234750@qq.com>
2023-12-25 17:14:57 +0800

    Add QwenVLPlus API

--- vlmeval/api/__init__.py
+++ vlmeval/api/__init__.py
@@ -2,6 +2,7 @@ from .gpt import OpenAIWrapper, GPT4V
 from .gpt_int import OpenAIWrapperInternal, GPT4V_Internal
 from .hf_chat_model import HFChatModel
 from .gemini import GeminiWrapper, GeminiProVision
+from .qwen_vl_plus import QwenVLWrapper,QwenVLPlus
 
 __all__ = [
     'OpenAIWrapper', 'HFChatModel', 'OpenAIWrapperInternal', 'GeminiWrapper',
