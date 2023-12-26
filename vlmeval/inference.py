23bad7381c2f6323178c758fd23c8b43031b0e57
kennymckormick <dhd@pku.edu.cn>
2023-12-26 13:40:45 +0800

    update API cont.

--- vlmeval/inference.py
+++ vlmeval/inference.py
@@ -33,6 +33,10 @@ def infer_data_api(model_name, dataset_name, index_set, api_nproc=4):
     structs = [dataset.build_prompt(data.iloc[i]) for i in range(lt)]
     
     out_file = f'{model_name}/{model_name}_{dataset_name}_supp.pkl'
+    res = load(out_file)
+    
+    structs = [s for i, s in zip(indices, structs) if i not in res]
+    indices = [i for i in indices if i not in res]
     
     gen_func = None
     if listinstr(['MMMU'], dataset_name):
