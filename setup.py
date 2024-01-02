65ae02a5c9eda89e031225abbea4c92a2dafd822
kennymckormick <dhd@pku.edu.cn>
2024-01-02 21:08:32 +0800

    update requirements

--- setup.py
+++ setup.py
@@ -1,40 +1,82 @@
+import re
+import sys
+from os.path import exists
 from setuptools import find_packages, setup
 
-REQUIRES = """
-numpy>=1.23.4
-openai==1.3.5
-requests
-tqdm
-pandas>=1.5.3
-tiktoken
-rich
-portalocker
-torch>=1.13.0
-timeout-decorator
-transformers==4.33.0
-opencv-python>=4.4.0.46
-pillow
-omegaconf
-matplotlib
-google-generativeai
-einops
-sentencepiece
-xformers
-termcolor
-xtuner
-SwissArmyTransformer 
-pycocoevalcap
-openpyxl
-seaborn
-tabulate
-xlsxwriter
-dashscope
-"""
+def parse_requirements(fname='requirements.txt', with_version=True):
+    """Parse the package dependencies listed in a requirements file but strips
+    specific versioning information.
 
+    Args:
+        fname (str): path to requirements file
+        with_version (bool, default=False): if True include version specs
 
-def get_install_requires():
-    reqs = [req for req in REQUIRES.split('\n') if len(req) > 0]
-    return reqs
+    Returns:
+        List[str]: list of requirements items
+
+    CommandLine:
+        python -c "import setup; print(setup.parse_requirements())"
+    """
+
+    require_fpath = fname
+
+    def parse_line(line):
+        """Parse information from a line in a requirements text file."""
+        if line.startswith('-r '):
+            # Allow specifying requirements in other files
+            target = line.split(' ')[1]
+            for info in parse_require_file(target):
+                yield info
+        else:
+            info = {'line': line}
+            if line.startswith('-e '):
+                info['package'] = line.split('#egg=')[1]
+            elif '@git+' in line:
+                info['package'] = line
+            else:
+                # Remove versioning from the package
+                pat = '(' + '|'.join(['>=', '==', '>']) + ')'
+                parts = re.split(pat, line, maxsplit=1)
+                parts = [p.strip() for p in parts]
+
+                info['package'] = parts[0]
+                if len(parts) > 1:
+                    op, rest = parts[1:]
+                    if ';' in rest:
+                        # Handle platform specific dependencies
+                        # http://setuptools.readthedocs.io/en/latest/setuptools.html#declaring-platform-specific-dependencies
+                        version, platform_deps = map(str.strip,
+                                                     rest.split(';'))
+                        info['platform_deps'] = platform_deps
+                    else:
+                        version = rest  # NOQA
+                    info['version'] = (op, version)
+            yield info
+
+    def parse_require_file(fpath):
+        with open(fpath, 'r') as f:
+            for line in f.readlines():
+                line = line.strip()
+                if line and not line.startswith('#'):
+                    for info in parse_line(line):
+                        yield info
+
+    def gen_packages_items():
+        if exists(require_fpath):
+            for info in parse_require_file(require_fpath):
+                parts = [info['package']]
+                if with_version and 'version' in info:
+                    parts.extend(info['version'])
+                if not sys.version.startswith('3.4'):
+                    # apparently package_deps are broken in 3.4
+                    platform_deps = info.get('platform_deps')
+                    if platform_deps is not None:
+                        parts.append(';' + platform_deps)
+                item = ''.join(parts)
+                yield item
+
+    packages = list(gen_packages_items())
+    return packages
 
 
 with open('README.md') as f:
@@ -46,12 +88,14 @@ def do_setup():
         name='vlmeval',
         version='0.1.0',
         description='OpenCompass VLM Evaluation Kit',
-        # url="",
         author="Haodong Duan",
+        author_email='dhd.efz@gmail.com',
+        maintainer='Haodong Duan',
+        maintainer_email='dhd.efz@gmail.com',
         long_description=readme,
         long_description_content_type='text/markdown',
         cmdclass={},
-        install_requires=get_install_requires(),
+        install_requires=parse_requirements('requirements.txt'),
         setup_requires=[],
         python_requires='>=3.7.0',
         packages=find_packages(exclude=[
