477da27a04ff8bc6849babfae43a446b7a977f29
kennymckormick <dhd@pku.edu.cn>
2024-01-11 10:45:38 +0800

    update

--- scripts/summarize.py
+++ scripts/summarize.py
@@ -1,18 +1,21 @@
 from vlmeval.smp import *
+from vlmeval.utils.dataset_config import dataset_URLs
 
 def get_score(model, dataset):
     file_name = f'{model}/{model}_{dataset}'
-    if listinstr(['CCBench', 'MMBench', 'SEEDBench_IMG'], dataset):
+    if listinstr(['CCBench', 'MMBench', 'SEEDBench_IMG', 'MMMU', 'ScienceQA'], dataset):
         file_name += '_acc.csv'
     elif listinstr(['MME', 'Hallusion'], dataset):
         file_name += '_score.csv'
-    elif listinstr(['MMVet'], dataset):
+    elif listinstr(['MMVet', 'MathVista'], dataset):
         file_name += '_gpt-4-turbo_score.csv'
     elif listinstr(['COCO'], dataset):
         file_name += '_score.json'
     else:
         raise NotImplementedError
     
+    if not osp.exists(file_name):
+        return {}
     data = load(file_name)
     ret = {}
     if dataset == 'CCBench':
@@ -29,22 +32,29 @@ def get_score(model, dataset):
                 ret['MMBench_DEV_CN'] = a * 100
             elif n == 'test':
                 ret['MMBench_TEST_CN'] = a * 100
-    elif 'MMBench' in dataset:
-        assert dataset in ['MMBench_DEV_EN', 'MMBench_TEST_EN', 'MMBench_DEV_CN', 'MMBench_TEST_CN']
+    elif listinstr(['SEEDBench', 'ScienceQA', 'MMBench'], dataset):
         ret[dataset] = data['Overall'][0] * 100
     elif 'MME' == dataset:
         ret[dataset] = data['perception'][0] + data['reasoning'][0]
     elif 'MMVet' == dataset:
-        for n, a in zip(data['Category'], data['acc']):
-            if n == 'Overall':
-                ret[dataset] = a
-    elif 'SEEDBench_IMG' == dataset:
-        ret[dataset] = data['Overall'][0] * 100
+        data = data[data['Category'] == 'Overall']
+        ret[dataset] = float(data['acc'][0])
+    elif 'HallusionBench' == dataset:
+        data = data[data['split'] == 'Overall']
+        for met in ['aAcc', 'qAcc', 'fAcc']:
+            ret[dataset + f' ({met})'] = float(data[met][0])
+    elif 'MMMU' in dataset:
+        data = data[data['split'] == 'validation']
+        ret['MMMU (val)'] = float(data['Overall'][0]) * 100
+    elif 'MathVista' in dataset:
+        data = data[data['Task&Skill'] == 'Overall']
+        ret[dataset] = float(data['acc'][0])
+     
     return ret
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data', type=str, nargs='+', required=True)
+    parser.add_argument('--data', type=str, nargs='+', default=[])
     parser.add_argument("--model", type=str, nargs='+', required=True)
     args = parser.parse_args()
     return args
@@ -77,4 +87,6 @@ def gen_table(models, datasets):
     
 if __name__ == '__main__':
     args = parse_args()
+    if args.data == []:
+        args.data = list(dataset_URLs)
     gen_table(args.model, args.data)
