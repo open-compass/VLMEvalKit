0bc1a98a8a9129b1695b75c95cc42bcaf1cbe6d3
kennymckormick <dhd@pku.edu.cn>
2024-01-23 16:54:34 +0800

    fix gradio port

--- app.py
+++ app.py
@@ -141,4 +141,4 @@ with gr.Blocks() as demo:
             ).style(show_copy_button=True)
 
 if __name__ == '__main__':
-    demo.launch(server_name='0.0.0.0', server_port=8000)
+    demo.launch(server_name='0.0.0.0')
