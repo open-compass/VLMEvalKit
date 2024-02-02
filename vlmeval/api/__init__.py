d160cc52bd323efdfe9a871eb3b80596741b5c7f
kennymckormick <dhd@pku.edu.cn>
2024-02-02 17:54:37 +0800

    support QwenAPI

--- vlmeval/api/__init__.py
+++ vlmeval/api/__init__.py
@@ -3,8 +3,10 @@ from .gpt_int import OpenAIWrapperInternal, GPT4V_Internal
 from .hf_chat_model import HFChatModel
 from .gemini import GeminiWrapper, GeminiProVision
 from .qwen_vl_api import QwenVLWrapper, QwenVLAPI
+from .qwen_api import QwenAPI
 
 __all__ = [
     'OpenAIWrapper', 'HFChatModel', 'OpenAIWrapperInternal', 'GeminiWrapper',
-    'GPT4V', 'GPT4V_Internal', 'GeminiProVision','QwenVLWrapper', 'QwenVLAPI'
+    'GPT4V', 'GPT4V_Internal', 'GeminiProVision','QwenVLWrapper', 'QwenVLAPI',
+    'QwenAPI'
 ]
