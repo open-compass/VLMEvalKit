5be2236430c2e999441c71547189917df0d1812e
kennymckormick <dhd@pku.edu.cn>
2024-02-22 13:51:44 +0800

    update

--- vlmeval/inference.py
+++ vlmeval/inference.py
@@ -207,7 +207,7 @@ def infer_data_job(model, work_dir, model_name, dataset_name, verbose=False, api
         else:
             dump(results, tmp_file)
         
-        res = infer_data_api(work_dir, model_name, dataset_name, api_nproc=4)
+        res = infer_data_api(work_dir, model_name, dataset_name, api_nproc=api_nproc)
         data = TSVDataset(dataset_name).data
         data['prediction'] = [str(res[x]) for x in data['index']]
         dump(data, result_file)
