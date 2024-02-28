b1d0fb2d0e12652cbfa363c32003f2e59fb490bc
kennymckormick <dhd@pku.edu.cn>
2024-02-28 14:32:26 +0800

    update inference

--- vlmeval/inference.py
+++ vlmeval/inference.py
@@ -26,8 +26,7 @@ def infer_data_api(work_dir, model_name, dataset_name, index_set=None, api_nproc
         data = data[data['index'].isin(index_set)]
 
     model = supported_VLM[model_name]() if isinstance(model_name, str) else model_name
-    is_api = getattr(model, 'is_api', False)
-    assert is_api
+    assert getattr(model, 'is_api', False)
     
     lt, indices = len(data), list(data['index'])
     structs = [dataset.build_prompt(data.iloc[i]) for i in range(lt)]
@@ -58,23 +57,27 @@ def infer_data_api(work_dir, model_name, dataset_name, index_set=None, api_nproc
         track_progress_rich(gen_func, structs, nproc=api_nproc, chunksize=api_nproc, save=out_file, keys=indices)
     
     res = load(out_file)
+    if index_set is not None:
+        res = {k: v for k, v in res.items() if k in index_set}
+    os.remove(out_file)
     return res
 
 def infer_data(model_name, work_dir, dataset_name, out_file, verbose=False, api_nproc=4):
-    res = {}
+    prev_file = f'{work_dir}/{model_name}_{dataset_name}_PREV.pkl'
+    res = load(prev_file) if osp.exists(prev_file) else {}
     if osp.exists(out_file):
-        res = load(out_file)
+        res.update(load(out_file))
 
     rank, world_size = get_rank_and_world_size()   
-    if rank == 0:
-        dataset = TSVDataset(dataset_name)
+    if rank == 0:dataset = TSVDataset(dataset_name)
     if world_size > 1:
         dist.barrier()
     dataset = TSVDataset(dataset_name)
 
-    indices = list(range(rank, len(dataset), world_size))
-    lt = len(indices)
-    data = dataset.data.iloc[indices]
+    sheet_indices = list(range(rank, len(dataset), world_size))
+    lt = len(sheet_indices)
+    data = dataset.data.iloc[sheet_indices]
+    data_indices = [i for i in data['index']]
 
     # If finished, will exit without building the model
     all_finished = True
@@ -82,8 +85,9 @@ def infer_data(model_name, work_dir, dataset_name, out_file, verbose=False, api_
         idx = data.iloc[i]['index']
         if idx not in res:
             all_finished = False
-    if all_finished:
-        return 
+    if all_finished: return 
+
+    # Data need to be inferred
     data = data[~data['index'].isin(res)]
     lt = len(data)
 
@@ -96,13 +100,13 @@ def infer_data(model_name, work_dir, dataset_name, out_file, verbose=False, api_
         for idx in indices:
             assert idx in supp
         res.update(supp)
+        res = {k: res[k] for k in data_indices}
         dump(res, out_file)
         return model_name
 
     for i in tqdm(range(lt)):
         idx = data.iloc[i]['index']
-        if idx in res:
-            continue
+        if idx in res: continue
 
         if hasattr(model, 'use_custom_prompt') and model.use_custom_prompt(dataset_name):
             struct = model.build_prompt(data.iloc[i], dataset=dataset_name)
@@ -130,6 +134,7 @@ def infer_data(model_name, work_dir, dataset_name, out_file, verbose=False, api_
         if (i + 1) % 20 == 0:
             dump(res, out_file)
 
+    res = {k: res[k] for k in data_indices}
     dump(res, out_file)
     return model
 
@@ -171,47 +176,33 @@ def infer_data_job(model, work_dir, model_name, dataset_name, verbose=False, api
     result_file = osp.join(work_dir, f'{model_name}_{dataset_name}.xlsx')
     is_api = model_name in api_models
 
-    if not osp.exists(result_file):
-        rank, world_size = get_rank_and_world_size()   
-        tmpl = osp.join(work_dir, '{}' + f'{world_size}_{dataset_name}.pkl')
-        out_file = tmpl.format(rank)
-
-        model = infer_data(model, work_dir=work_dir, dataset_name=dataset_name, out_file=out_file, verbose=verbose)
-        if world_size > 1: dist.barrier()
-
-        if rank == 0:
-            data_all = {}
-            for i in range(world_size):
-                data_all.update(load(tmpl.format(i)))
-
-            data = TSVDataset(dataset_name).data
-            for x in data['index']:
-                assert x in data_all
-            data['prediction'] = [str(data_all[x]) for x in data['index']]
-            data.pop('image')
-            
-            dump(data, result_file)             
-            for i in range(world_size):
-                os.remove(tmpl.format(i))
-        return model
-    elif is_api:
-        tmp_file = f'{work_dir}/{model_name}_{dataset_name}_supp.pkl'
+    prev_file = f'{work_dir}/{model_name}_{dataset_name}_PREV.pkl'
+    if osp.exists(result_file):
         data = load(result_file)
         results = {k: v for k, v in zip(data['index'], data['prediction'])}
         if not ignore_failed:
             results = {k: v for k, v in results.items() if FAIL_MSG not in str(v)}
-        if osp.exists(tmp_file):
-            old_res = load(tmp_file)
-            old_res.update(results)
-            dump(old_res, tmp_file)
-        else:
-            dump(results, tmp_file)
-        
-        res = infer_data_api(work_dir, model_name, dataset_name, api_nproc=api_nproc, ignore_failed=ignore_failed)
+        dump(results, prev_file)
+
+    rank, world_size = get_rank_and_world_size()   
+    tmpl = osp.join(work_dir, '{}' + f'{world_size}_{dataset_name}.pkl')
+    out_file = tmpl.format(rank)
+
+    model = infer_data(model, work_dir=work_dir, dataset_name=dataset_name, out_file=out_file, verbose=verbose)
+    if world_size > 1: dist.barrier()
+
+    if rank == 0:
+        data_all = {}
+        for i in range(world_size):
+            data_all.update(load(tmpl.format(i)))
+
         data = TSVDataset(dataset_name).data
-        data['prediction'] = [str(res[x]) for x in data['index']]
-        dump(data, result_file)
-        return model_name
-    else:
-        logger = get_logger("Inference")
-        logger.error("Result file already exists (HF Models). Please remove it first. ")
+        for x in data['index']:
+            assert x in data_all
+        data['prediction'] = [str(data_all[x]) for x in data['index']]
+        data.pop('image')
+        
+        dump(data, result_file)             
+        for i in range(world_size):
+            os.remove(tmpl.format(i))
+    return model
