dabd92913470d6bb0bda58a25f98c0209a3b18c9
Xinyu Fang <fangxinyutju202009@126.com>
2024-03-06 17:11:51 +0800

    Update vqa_eval.py
    modify vqa score calculation in hit_calculate function

--- vlmeval/evaluate/vqa_eval.py
+++ vlmeval/evaluate/vqa_eval.py
@@ -158,16 +158,16 @@ def _process_digit_article(inText):
     return outText
 
 
-def hit_calculate(result, dataset_name, full_score_weight=0.3, anls_threshold = 0.5):
+def hit_calculate(result, dataset_name, vqa_score_threshold = 3, anls_threshold = 0.5):
     if listinstr(['TextVQA'], dataset_name):
-        return [np.mean(x['match']) >= full_score_weight for x in result]
+        return [min(np.sum(x['match'])/vqa_score_threshold, 1) for x in result]
     elif listinstr(['DocVQA'], dataset_name): 
         # return [1 - np.min(x['match']) >= anls_threshold for x in result]
         return [0.0 if 1 - np.min(x['match']) < anls_threshold else 1 - np.min(x['match']) for x in result ]
     elif listinstr(['ChartQA','OCRVQA'], dataset_name):
         return [np.max(x['match']) for x in result]
     else: #default using vqa_score to calculate score
-        return [np.mean(x['match']) >= full_score_weight for x in result]
+        return [min(np.sum(x['match'])/vqa_score_threshold, 1) for x in result]
 
 # https://github.com/google-research/pix2struct/blob/main/pix2struct/metrics.py#L81
 def relaxed_correctness(target: str,
