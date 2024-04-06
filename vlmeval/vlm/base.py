6038d3535e625769ca57500fc87c62cc9507ed1d
kennymckormick <dhd@pku.edu.cn>
2024-04-06 17:35:20 +0800

    update the baseclass of opensource VLM

--- vlmeval/vlm/base.py
+++ vlmeval/vlm/base.py
@@ -6,13 +6,40 @@ from abc import abstractmethod
 class BaseModel:
 
     def use_custom_prompt(self, dataset):
+        """Whether to use custom prompt for the given dataset.
+
+        Args:
+            dataset (str): The name of the dataset.
+
+        Returns:
+            bool: Whether to use custom prompt. If True, will call `build_prompt` of the VLM to build the prompt.
+                Default to False.
+        """
         return False
 
     @abstractmethod
     def build_prompt(self, line, dataset):
+        """Build custom prompts for a specific dataset. Called only if `use_custom_prompt` returns True.
+
+        Args:
+            line (line of pd.DataFrame): The raw input line.
+            dataset (str): The name of the dataset.
+
+        Returns:
+            str: The built message.
+        """
         raise NotImplementedError
 
     def dump_image(self, line, dataset):
+        """Dump the image(s) of the input line to the corresponding dataset folder.
+
+        Args:
+            line (line of pd.DataFrame): The raw input line.
+            dataset (str): The name of the dataset.
+
+        Returns:
+            str | list[str]: The path(s) of the dumped image(s).
+        """
         ROOT = LMUDataRoot()
         assert isinstance(dataset, str)
         img_root = osp.join(ROOT, 'images', img_root_map[dataset] if dataset in img_root_map else dataset)
@@ -32,5 +59,14 @@ class BaseModel:
         return tgt_path
 
     @abstractmethod
-    def generate(self, msgs, dataset=None):
+    def generate(self, message, dataset=None):
+        """Generate the output message.
+
+        Args:
+            message (list[dict]): The input message.
+            dataset (str, optional): The name of the dataset. Defaults to None.
+
+        Returns:
+            str: The generated message.
+        """
         raise NotImplementedError
