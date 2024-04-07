8bd7cdaffa650ce952c296326ee5abebe18a23e5
kennymckormick <dhd@pku.edu.cn>
2024-04-07 17:07:24 +0800

    update check_VLM

--- scripts/check_VLM.py
+++ scripts/check_VLM.py
@@ -1,28 +1,29 @@
 import sys
 from vlmeval.smp import *
 from vlmeval.config import transformer_ver, supported_VLM
+PTH = osp.realpath(__file__)
+IMAGE_PTH = osp.join(PTH, '../assets/apple.jpg')
 
 msg1 = [
-    'https://raw.githubusercontent.com/open-compass/VLMEvalKit/main/assets/apple.jpg', 
+    IMAGE_PTH, 
     'What is in this image?'
 ]
 msg2 = [
-    dict(type='image', value='https://raw.githubusercontent.com/open-compass/VLMEvalKit/main/assets/apple.jpg'),
+    dict(type='image', value=IMAGE_PTH),
     dict(type='text', value='What is in this image?')
 ]
 msg3 = [
-    'https://raw.githubusercontent.com/open-compass/VLMEvalKit/main/assets/apple.jpg', 
-    'https://raw.githubusercontent.com/open-compass/VLMEvalKit/main/assets/apple.jpg', 
+    IMAGE_PTH, 
+    IMAGE_PTH, 
     'How many apples are there in these images?'
 ]
 msg4 = [
-    dict(type='image', value='https://raw.githubusercontent.com/open-compass/VLMEvalKit/main/assets/apple.jpg'),
-    dict(type='image', value='https://raw.githubusercontent.com/open-compass/VLMEvalKit/main/assets/apple.jpg'),
+    dict(type='image', value=IMAGE_PTH),
+    dict(type='image', value=IMAGE_PTH),
     dict(type='text', value='How many apples are there in these images?')
 ]
 
 def check_VLM():
-    pth = osp.realpath(__file__)
     val = sys.argv[1]
     if val in supported_VLM:
         model = supported_VLM[val]()
@@ -35,7 +36,7 @@ def check_VLM():
     elif val in transformer_ver:
         models = transformer_ver[val]
         for m in models:
-            res = run_command(f'python {pth} {m}')
+            res = run_command(f'python {PTH} {m}')
 
 if __name__ == '__main__':
     check_VLM()
