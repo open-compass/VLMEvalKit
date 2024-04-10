6833e65f2913ca1d289900451fd213e530b490be
kennymckormick <dhd@pku.edu.cn>
2024-04-10 11:25:01 +0800

    bug fix

--- vlmeval/vlm/llava/llava.py
+++ vlmeval/vlm/llava/llava.py
@@ -3,9 +3,9 @@ from PIL import Image
 from abc import abstractproperty
 import sys
 import os.path as osp
-from .base import BaseModel
-from ..smp import *
-from ..utils import DATASET_TYPE
+from ..base import BaseModel
+from ...smp import *
+from ...utils import DATASET_TYPE
 
 
 class LLaVA(BaseModel):
