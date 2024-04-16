68b5f3f517f7a4e37c0dd295921408eb0f436505
kennymckormick <dhd@pku.edu.cn>
2024-04-16 16:24:00 +0800

    fix build_judge

--- vlmeval/evaluate/misc.py
+++ vlmeval/evaluate/misc.py
@@ -5,7 +5,7 @@ from vlmeval.smp import load_env
 INTERNAL = os.environ.get('INTERNAL', 0)
 
 
-def build_judge(version, **kwargs):
+def build_judge(model, **kwargs):
     load_env()
     LOCAL_LLM = os.environ.get('LOCAL_LLM', None)
     if LOCAL_LLM is None:
@@ -18,7 +18,7 @@ def build_judge(version, **kwargs):
             'chatgpt-0613': 'gpt-3.5-turbo-0613',
             'chatgpt-0125': 'gpt-3.5-turbo-0125'
         }
-        model_version = model_map[version]
+        model_version = model_map[model]
     else:
         model_version = LOCAL_LLM
     if INTERNAL:
