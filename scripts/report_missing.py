936192e47465ef7a5ce0bc0a2107d439b040ec4c
kennymckormick <dhd@pku.edu.cn>
2024-05-10 15:37:10 +0800

    update report_missing

--- scripts/report_missing.py
+++ scripts/report_missing.py
@@ -12,7 +12,8 @@ ESSENTIAL = [
     ('CCBench', 'acc.csv'), ('MMBench_CN', 'acc.csv'), ('MMVet', 'gpt-4-turbo_score.csv'),
     ('MMMU_DEV_VAL', 'acc.csv'), ('MathVista_MINI', 'gpt-4-turbo_score.csv'), ('HallusionBench', 'score.csv'),
     ('AI2D_TEST', 'acc.csv'), ('LLaVABench', 'score.csv'), ('OCRBench', 'score.json'),
-    ('MMStar', 'acc.csv'), ('RealWorldQA', 'acc.csv')
+    ('MMStar', 'acc.csv'), ('RealWorldQA', 'acc.csv'), ('MMBench_V11', 'acc.csv'), 
+    ('MMBench_CN_V11', 'acc.csv'), ('POPE', 'score.csv')
 ]
 OPTIONAL = [
     ('OCRVQA_TESTCORE', 'acc.csv'), ('TextVQA_VAL', 'acc.csv'), ('ChartQA_VALTEST_HUMAN', 'acc.csv'), 
