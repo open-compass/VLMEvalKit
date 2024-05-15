ae16405c3b59de3550b1a52e760e9e1be7676cbc
Junming Yang <60545459+junming-yang@users.noreply.github.com>
2024-05-15 20:16:33 +0800

    Fix message_to_promptimg

--- vlmeval/vlm/base.py
+++ vlmeval/vlm/base.py
@@ -144,10 +144,7 @@ class BaseModel:
         if num_images == 0:
             prompt = '\n'.join([x['value'] for x in message if x['type'] == 'text'])
             image = None
-        elif num_images == 1:
-            prompt = '\n'.join([x['value'] for x in message if x['type'] == 'text'])
-            image = [x['value'] for x in message if x['type'] == 'image'][0]
         else:
-            prompt = '\n'.join([x['value'] if x['type'] == 'text' else '<image>' for x in message])
+            prompt = '\n'.join([x['value'] for x in message if x['type'] == 'text'])
             image = [x['value'] for x in message if x['type'] == 'image'][0]
         return prompt, image
