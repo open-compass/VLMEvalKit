673d227ed01583ddee1d27bfd95e529867311944
Junming Yang <60545459+junming-yang@users.noreply.github.com>
2024-05-24 19:30:40 +0800

    update

--- vlmeval/vlm/phi3_vision.py
+++ vlmeval/vlm/phi3_vision.py
@@ -6,7 +6,7 @@ from ..smp import *
 
 
 class Phi3Vision(BaseModel):
-    
+
     INSTALL_REQ = False
     INTERLEAVE = False
 
@@ -17,7 +17,7 @@ class Phi3Vision(BaseModel):
             warnings.warn('Please install the latest version transformers.')
             sys.exit(-1)
         model = AutoModelForCausalLM.from_pretrained(
-            model_path, device_map="cuda", trust_remote_code=True, torch_dtype="auto").eval()
+            model_path, device_map='cuda', trust_remote_code=True, torch_dtype='auto').eval()
         processor = AutoProcessor.from_pretrained(model_path, trust_remote_code=True)
         self.model = model
         self.processor = processor
@@ -26,20 +26,28 @@ class Phi3Vision(BaseModel):
     def generate_inner(self, message, dataset=None):
         prompt, image_path = self.message_to_promptimg(message)
         image = Image.open(image_path).convert('RGB')
-        messages = [ 
-            {"role": "user", "content": f"<|image_1|>\n{prompt}"}
-        ] 
+        messages = [
+            {'role': 'user', 'content': f'<|image_1|>\n{prompt}'}
+        ]
         prompt = self.processor.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-        inputs = self.processor(prompt, [image], return_tensors="pt").to('cuda')
+        inputs = self.processor(prompt, [image], return_tensors='pt').to('cuda')
 
-        generation_args = { 
-            "max_new_tokens": 500, 
-            "temperature": 0.0, 
-            "do_sample": False, 
+        generation_args = {
+            'max_new_tokens': 500,
+            'temperature': 0.0,
+            'do_sample': False,
         }
         generation_args.update(self.kwargs)
 
-        generate_ids = self.model.generate(**inputs, eos_token_id=self.processor.tokenizer.eos_token_id, **generation_args) 
+        generate_ids = self.model.generate(
+            **inputs,
+            eos_token_id=self.processor.tokenizer.eos_token_id,
+            **generation_args
+        )
         generate_ids = generate_ids[:, inputs['input_ids'].shape[1]:]
-        response = self.processor.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0] 
+        response = self.processor.batch_decode(
+            generate_ids,
+            skip_special_tokens=True,
+            clean_up_tokenization_spaces=False
+        )[0]
         return response
