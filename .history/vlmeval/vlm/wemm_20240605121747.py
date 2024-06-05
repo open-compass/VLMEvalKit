3d49c7ab3146f2ca785826b2cf0e3b01c8e7aef0
FeipengMa6 <mafp@mail.ustc.edu.cn>
2024-06-05 12:19:41 +0800

    [Model] Add Support for WeMM

--- .history/vlmeval/vlm/wemm_20240605121747.py
+++ .history/vlmeval/vlm/wemm_20240605121747.py
@@ -1,19 +1,22 @@
 import torch
 from PIL import Image
+from abc import abstractproperty
 import sys
+import os.path as osp
 from ..smp import *
 from .base import BaseModel
-from ..utils import DATASET_TYPE
-from transformers import AutoModel
+from ..utils import DATASET_TYPE, CustomPrompt
+from transformers import AutoModel,AutoModelForCausalLM
+from transformers import AutoModel, AutoModelForCausalLM, AutoTokenizer, GenerationConfig, AutoConfig
 
 
 class WeMM(BaseModel):
-    def __init__(self, model_path='feipengma/WeMM', **kwargs):
+    def __init__(self, model_path="feipengma/WeMM", **kwargs):
         self.wemm = AutoModel.from_pretrained(model_path, torch_dtype=torch.bfloat16, trust_remote_code=True)
         self.wemm.cuda()
         self.wemm.eval()
         torch.cuda.empty_cache()
-
+        
     def use_custom_prompt(self, dataset):
         assert dataset is not None
         if DATASET_TYPE(dataset) == 'multi-choice':
@@ -44,17 +47,20 @@ class WeMM(BaseModel):
             )
         else:
             prompt += '\n请直接回答问题。' if cn_string(prompt) else '\nAnswer the question directly.'
-
+        
         message = [dict(type='text', value=prompt)]
         message.extend([dict(type='image', value=p) for p in tgt_path])
         return message
-
+        # return {'image': tgt_path, 'text': prompt}
+    
     def generate_inner(self, message, dataset=None):
         prompt, image_path = self.message_to_promptimg(message)
-
-        if dataset == 'HallusionBench':
-            prompt = prompt + ' Please answer yes or no. Answer the question using a single word or phrase.'
-
-        pred = self.wemm.mm_generate(image_path, prompt)
-
+        
+        if(dataset == 'HallusionBench'):
+            prompt = prompt + " Please answer yes or no. Answer the question using a single word or phrase."
+        
+        gen_config = None
+        pred = self.wemm.mm_generate(image_path, prompt, gen_config)
+        print(f"prompt is {prompt}\n answer is {pred}")
+        
         return pred
