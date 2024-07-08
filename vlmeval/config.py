e20884d84e98a0c3ce96d7d297adf5b77171fe32
LightDXY <dlight@mail.ustc.edu.cn>
2024-07-08 14:00:56 +0800

    add xcomposer 2.5

--- vlmeval/config.py
+++ vlmeval/config.py
@@ -124,6 +124,7 @@ xcomposer_series = {
     'XComposer2': partial(XComposer2, model_path='internlm/internlm-xcomposer2-vl-7b'),
     'XComposer2_1.8b': partial(XComposer2, model_path='internlm/internlm-xcomposer2-vl-1_8b'),
     'XComposer2_4KHD': partial(XComposer2_4KHD, model_path='internlm/internlm-xcomposer2-4khd-7b'),
+    'XComposer2d5': partial(XComposer2, model_path='internlm/internlm-xcomposer2d5-7b'),
 }
 
 minigpt4_series = {
