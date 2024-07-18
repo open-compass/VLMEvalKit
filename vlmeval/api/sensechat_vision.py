c36135ab1c0d7c4ee5eccc8e6edbaad074fc62d2
Junming Yang <60545459+junming-yang@users.noreply.github.com>
2024-07-18 19:38:39 +0800

    fix minor typo

--- vlmeval/api/sensechat_vision.py
+++ vlmeval/api/sensechat_vision.py
@@ -118,7 +118,7 @@ class SenseChatVisionWrapper(BaseAPI):
         elif dataset is not None and listinstr(['HallusionBench'], dataset):
             question = line['question']
             prompt = question + ' Please answer yes or no. Answer the question using a single word or phrase.'
-        elif dataset is not None and DATASET_TYPE(dataset) == 'multi-choice' and 'MMMU' not in dataset:
+        elif dataset is not None and DATASET_TYPE(dataset) == 'MCQ' and 'MMMU' not in dataset:
             prompt = self.build_multi_choice_prompt(line, dataset)
         elif dataset is not None and DATASET_TYPE(dataset) == 'VQA':
             if 'MathVista' in dataset:
