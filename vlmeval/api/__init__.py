68bef002a75b541e18baab858acd1081c95618b4
kennymckormick <dhd@pku.edu.cn>
2024-07-25 14:17:05 +0800

    remove stepai_int

--- vlmeval/api/__init__.py
+++ vlmeval/api/__init__.py
@@ -3,7 +3,6 @@ from .hf_chat_model import HFChatModel
 from .gemini import GeminiWrapper, GeminiProVision
 from .qwen_vl_api import QwenVLWrapper, QwenVLAPI
 from .qwen_api import QwenAPI
-from .stepai import Step1V_INT
 from .claude import Claude_Wrapper, Claude3V
 from .reka import Reka
 from .glm_vision import GLMVisionAPI
@@ -13,6 +12,6 @@ from .sensechat_vision import SenseChatVisionAPI
 __all__ = [
     'OpenAIWrapper', 'HFChatModel', 'GeminiWrapper', 'GPT4V',
     'GeminiProVision', 'QwenVLWrapper', 'QwenVLAPI', 'QwenAPI',
-    'Claude3V', 'Claude_Wrapper', 'Reka', 'Step1V_INT', 'GLMVisionAPI',
+    'Claude3V', 'Claude_Wrapper', 'Reka', 'GLMVisionAPI',
     'CWWrapper', 'SenseChatVisionAPI'
 ]
