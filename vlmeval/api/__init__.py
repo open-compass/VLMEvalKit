5bf1d3b7b6544c70a44dd87b80996066a9e003b1
kennymckormick <dhd@pku.edu.cn>
2024-07-25 10:30:09 +0800

    remove gpt_int

--- vlmeval/api/__init__.py
+++ vlmeval/api/__init__.py
@@ -1,5 +1,4 @@
 from .gpt import OpenAIWrapper, GPT4V
-from .gpt_int import OpenAIWrapperInternal, GPT4V_Internal
 from .hf_chat_model import HFChatModel
 from .gemini import GeminiWrapper, GeminiProVision
 from .qwen_vl_api import QwenVLWrapper, QwenVLAPI
@@ -12,8 +11,8 @@ from .cloudwalk import CWWrapper
 from .sensechat_vision import SenseChatVisionAPI
 
 __all__ = [
-    'OpenAIWrapper', 'HFChatModel', 'OpenAIWrapperInternal', 'GeminiWrapper',
-    'GPT4V', 'GPT4V_Internal', 'GeminiProVision', 'QwenVLWrapper', 'QwenVLAPI',
-    'QwenAPI', 'Claude3V', 'Claude_Wrapper', 'Reka', 'Step1V_INT', 'GLMVisionAPI',
+    'OpenAIWrapper', 'HFChatModel', 'GeminiWrapper', 'GPT4V',
+    'GeminiProVision', 'QwenVLWrapper', 'QwenVLAPI', 'QwenAPI',
+    'Claude3V', 'Claude_Wrapper', 'Reka', 'Step1V_INT', 'GLMVisionAPI',
     'CWWrapper', 'SenseChatVisionAPI'
 ]
