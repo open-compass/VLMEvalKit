3c5fb3084cc6fb59699ce09056b298c95076ea79
Junming Yang <60545459+junming-yang@users.noreply.github.com>
2024-08-02 13:18:48 +0800

    Update mantis.py

--- vlmeval/vlm/mantis.py
+++ vlmeval/vlm/mantis.py
@@ -1,4 +1,3 @@
-import PIL
 import torch
 from PIL import Image
 from abc import abstractproperty
@@ -7,41 +6,8 @@ from ..smp import *
 from ..dataset import DATASET_TYPE
 import warnings
 
-try:
-    from mantis.models.mllava import LlavaForConditionalGeneration, MLlavaProcessor
-    from mantis.models.mfuyu import MFuyuForCausalLM, MFuyuProcessor
-    from mantis.models.conversation import conv_mllava_v1 as default_conv, conv_templates
-
-except Exception as e:
-    warnings.warn(
-        "Mantis is not installed. Please install Mantis to use this model.Please use 'pip install "
-        "git+https://github.com/TIGER-AI-Lab/Mantis.git' to install\nError: %s" % e)
-
-try:
-    from transformers import AutoModelForVision2Seq, AutoProcessor
-except Exception as e:
-    warnings.warn("Upgrade transformers to use Mantis's idefics model.\nError: %s" % e)
-except:
-    warnings.warn('Please `pip install git+https://github.com/LLaVA-VL/LLaVA-NeXT.git')
-
-# inference implementation for attention, can be "sdpa", "eager", "flash_attention_2".
-# Seems FA2 is not effective during inference:
-# https://discuss.huggingface.co/t/flash-attention-has-no-effect-on-inference/73453/5
-
-# if is_flash_attn_2_available:
-#     best_fit_attn_implementation = "flash_attention_2"
-# flash_attn has a bug that says: ERROR Error query and key must have the same dtype in generating
-
-try:
-    import flash_attn
-
-    best_fit_attn_implementation = 'flash_attention_2'
-except ImportError:
-    best_fit_attn_implementation = 'eager'
 
 class Mantis(BaseModel):
-
-    
     """
     Mantis Model
     This implementation is adpated from the Llava model from llava.py and the Idefics model from idefics.py
@@ -54,7 +20,35 @@ class Mantis(BaseModel):
 
     def __init__(self, model_path='TIGER-Lab/Mantis-8B-siglip-llama3', **kwargs):
         assert model_path is not None
+        try:
+            from mantis.models.mllava import LlavaForConditionalGeneration, MLlavaProcessor
+            from mantis.models.mfuyu import MFuyuForCausalLM, MFuyuProcessor
+            from mantis.models.conversation import conv_mllava_v1 as default_conv, conv_templates
+        except:
+            warnings.warn(
+                "Mantis is not installed. Please install Mantis to use this model.Please use 'pip install "
+                "git+https://github.com/TIGER-AI-Lab/Mantis.git' to install"
+            )
 
+        try:
+            from transformers import AutoModelForVision2Seq, AutoProcessor
+        except Exception as e:
+            warnings.warn("Upgrade transformers to use Mantis's idefics model.\nError: %s" % e)
+        except:
+            warnings.warn('Please `pip install git+https://github.com/LLaVA-VL/LLaVA-NeXT.git')
+
+        # inference implementation for attention, can be "sdpa", "eager", "flash_attention_2". 
+        # Seems FA2 is not effective during inference: 
+        # https://discuss.huggingface.co/t/flash-attention-has-no-effect-on-inference/73453/5
+        # if is_flash_attn_2_available:
+        #     best_fit_attn_implementation = "flash_attention_2" 
+        # flash_attn has a bug that says: ERROR Error query and key must have the same dtype in generating
+
+        try:
+            import flash_attn
+            best_fit_attn_implementation = 'flash_attention_2'
+        except ImportError:
+            best_fit_attn_implementation = 'eager'
         self.model_path = model_path
         attn_implementation = best_fit_attn_implementation
         self._is_idefics = 'idefics' in model_path.lower()
@@ -62,18 +56,28 @@ class Mantis(BaseModel):
         if not self._is_idefics:
             if 'fuyu' in model_path.lower():
                 self.processor = MFuyuProcessor.from_pretrained(self.model_path)
-                model = MFuyuForCausalLM.from_pretrained(self.model_path, device_map='cuda',
-                                                         attn_implementation=attn_implementation,
-                                                         torch_dtype=torch.float16)
+                model = MFuyuForCausalLM.from_pretrained(
+                    self.model_path,
+                    device_map='cuda',
+                    attn_implementation=attn_implementation,
+                    torch_dtype=torch.float16
+                )
             else:
                 self.processor = MLlavaProcessor.from_pretrained(self.model_path)
-                model = LlavaForConditionalGeneration.from_pretrained(self.model_path, device_map='cuda',
-                                                                      attn_implementation=attn_implementation,
-                                                                      torch_dtype=torch.float16)
+                model = LlavaForConditionalGeneration.from_pretrained(
+                    self.model_path,
+                    device_map='cuda',
+                    attn_implementation=attn_implementation,
+                    torch_dtype=torch.float16
+                )
         else:
             self.processor = AutoProcessor.from_pretrained(self.model_path)
-            model = AutoModelForVision2Seq.from_pretrained(self.model_path, device_map='cuda',
-                                                           torch_dtype=torch.float16)
+            model = AutoModelForVision2Seq.from_pretrained(
+                self.model_path,
+                device_map='cuda',
+                torch_dtype=torch.float16
+            )
+
         model = model.eval()
         self.model = model.cuda()
         kwargs_default = dict(do_sample=False, temperature=0, max_new_tokens=1024, top_p=None, num_beams=1)
@@ -82,6 +86,8 @@ class Mantis(BaseModel):
         warnings.warn(f'Following kwargs received: {self.kwargs}, will use as generation config. ')
 
         self.tokenizer = self.processor.tokenizer
+        self.default_conv = default_conv
+        self.conv_templates = conv_templates
 
     def use_custom_prompt(self, dataset):
         assert dataset is not None
@@ -129,6 +135,7 @@ class Mantis(BaseModel):
             answer = answer.split('assistant\n')[1].strip()
         elif '<|end_header_id|>\n\n' in answer:
             answer = answer.split('<|end_header_id|>\n\n')[2].strip()
+
         if '</s>' in answer:
             answer = answer.split('</s>')[0].strip()
         elif '<|im_end|>' in answer:
@@ -155,21 +162,23 @@ class Mantis(BaseModel):
             prompt = [{'role': 'user', 'content': content}]
             prompt = self.processor.apply_chat_template(prompt, add_generation_prompt=True)
         else:
-            # Follow Mantis code base: https://github.com/TIGER-AI-Lab/Mantis/blob/main/mantis/models/mllava/utils.py
-            # #L33 to make sure they are consistent Users don't need to define chat template as it is done here
+            # Follow the Mantis code base to make sure they are consistent: 
+            # https://github.com/TIGER-AI-Lab/Mantis/blob/main/mantis/models/mllava/utils.py#L33
+            # Users don't need to define chat template as it is done here
             if 'llama-3' in self.model.language_model.name_or_path.lower():
-                conv = conv_templates['llama_3']
+                conv = self.conv_templates['llama_3']
                 terminators = [
                     self.processor.tokenizer.eos_token_id,
                     self.processor.tokenizer.convert_tokens_to_ids('<|eot_id|>')
                 ]
             else:
-                conv = default_conv
+                conv = self.default_conv
                 terminators = None
 
             # Using EOT because end of *text* is more accurate for what we're doing than end of *sentence*
             if 'eos_token_id' not in self.kwargs:
                 self.kwargs['eos_token_id'] = terminators
+
             conv = conv.copy()
             conv.append_message(conv.roles[0], content)
             conv.append_message(conv.roles[1], '')
@@ -177,8 +186,8 @@ class Mantis(BaseModel):
             prompt = conv.get_prompt()
 
         inputs = self.processor(prompt, images, return_tensors='pt', truncation=True)
-        if 'image_patches' in inputs.keys():
-            # FIXME: Fuyu model would return a list instead of a pytorch tensor. This weird behavior needs fixing.
+        # FIXME: Fuyu model would return a list instead of a pytorch tensor. This weird behavior needs fixing.
+        if 'image_patches' in inputs.keys():  
             inputs['image_patches'] = inputs['image_patches'][0]
         inputs = {k: v.to(self.model.device) for k, v in inputs.items()}
         output = self.model.generate(**inputs, **self.kwargs)
