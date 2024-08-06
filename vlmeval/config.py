74bde990b02c349e1394accbd45d6e9bf224f667
lihytotoro <hy-li22@mails.tsinghua.edu.cn>
2024-08-06 19:40:54 +0800

    add MiniCPM-V 2.6

--- vlmeval/config.py
+++ vlmeval/config.py
@@ -75,6 +75,7 @@ minicpm_series = {
     'MiniCPM-V': partial(MiniCPM_V, model_path='openbmb/MiniCPM-V'),
     'MiniCPM-V-2': partial(MiniCPM_V, model_path='openbmb/MiniCPM-V-2'),
     'MiniCPM-Llama3-V-2_5': partial(MiniCPM_Llama3_V, model_path='openbmb/MiniCPM-Llama3-V-2_5'),
+    'MiniCPM-V-2_6': partial(MiniCPM_V_2_6, model_path='openbmb/MiniCPM-V-2_6'),
 }
 
 xtuner_series = {
