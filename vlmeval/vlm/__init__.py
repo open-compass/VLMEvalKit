1dbf75150f6da91d45fc4c0fc629b043519ef0d4
lihytotoro <hy-li22@mails.tsinghua.edu.cn>
2024-08-07 15:43:38 +0800

    Add MiniCPM-V-2_6

--- vlmeval/vlm/__init__.py
+++ vlmeval/vlm/__init__.py
@@ -8,7 +8,7 @@ from .emu import Emu
 from .idefics import IDEFICS, IDEFICS2
 from .instructblip import InstructBLIP
 from .llava import LLaVA, LLaVA_Next, LLaVA_XTuner, LLaVA_Next2
-from .minicpm_v import MiniCPM_V, MiniCPM_Llama3_V
+from .minicpm_v import MiniCPM_V, MiniCPM_Llama3_V, MiniCPM_V_2_6
 from .minigpt4 import MiniGPT4
 from .mmalaya import MMAlaya
 from .monkey import Monkey, MonkeyChat
