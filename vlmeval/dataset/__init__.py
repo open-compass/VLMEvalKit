fb626f06afec64167d1e96f601d18b7c88fa1424
kennymckormick <dhd@pku.edu.cn>
2024-08-17 22:01:03 +0800

    BUG FIX

--- vlmeval/dataset/__init__.py
+++ vlmeval/dataset/__init__.py
@@ -135,7 +135,7 @@ def DATASET_TYPE(dataset):
 
 
 def build_dataset(dataset_name, **kwargs):
-    for cls in SUPPORTED_DATASETS:
+    for cls in DATASET_CLASSES:
         if dataset_name in cls.supported_datasets():
             return cls(dataset=dataset_name, **kwargs)
 
