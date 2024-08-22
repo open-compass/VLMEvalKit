48cb31fbfd1ec2bae6af6dc704cabda868a2c5ad
SYuan03 <211250097@smail.nju.edu.cn>
2024-08-22 12:45:31 +0800

    remove demo.py

--- demo.py
+++
@@ -1,7 +0,0 @@
-# Demo
-from vlmeval.config import supported_VLM
-model = supported_VLM['mPLUG-Owl3']()
-
-# Forward Multiple Images
-ret = model.generate(['assets/apple.jpg', 'assets/apple.jpg', 'How many apples are there in the provided images? '])
-print(ret)  # There are two apples in the provided images.
