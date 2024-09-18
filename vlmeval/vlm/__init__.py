1b07b0954140d82e21678e2f763f215fa9e17969
root <running.lsy@alibaba-inc.com>
2024-09-18 14:38:17 +0800

    fix missing import

--- vlmeval/vlm/__init__.py
+++ vlmeval/vlm/__init__.py
@@ -36,7 +36,7 @@ from .cambrian import Cambrian
 from .chameleon import Chameleon
 from .video_llm import VideoLLaVA, VideoLLaVA_HF, Chatunivi, VideoChatGPT, LLaMAVID, VideoChat2_HD, PLLaVA
 from .vila import VILA
-from .ovis import Ovis
+from .ovis import Ovis, Ovis1_6
 from .mantis import Mantis
 from .mixsense import LLama3Mixsense
 from .parrot import Parrot
