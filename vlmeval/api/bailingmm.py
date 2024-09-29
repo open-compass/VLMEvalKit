75a4e9eb1fa7d375fdfb8d5e9c39199e3057c7d1
zhengchuanyang <zhengchuanyang.zcy@ant.com>
2024-09-29 12:11:38 +0800

    add BailingMM

--- vlmeval/api/bailingmm.py
+++ vlmeval/api/bailingmm.py
@@ -3,7 +3,7 @@ from vlmeval.smp import *
 from vlmeval.api.base import BaseAPI
 from vlmeval.dataset import DATASET_TYPE
 from vlmeval.smp.vlm import encode_image_file_to_base64
-
+import time
 
 class BailingMMWrapper(BaseAPI):
 
@@ -22,13 +22,11 @@ class BailingMMWrapper(BaseAPI):
 
         self.model = model
         self.fail_msg = 'Failed to obtain answer via BailingMM API.'
-        # if key is None:
-        #     key = os.environ.get('GLMV_API_KEY', None)
-        # assert key is not None, (
-        #     'Please set the API Key'
-        # )
+        if key is None:
+            key = os.environ.get('BAILINGMM_API_KEY', None)
+        assert key is not None, ('Please set the API Key for BailingMM.')
+        self.key = key
         self.headers = {"Content-Type": "application/json"}
-        # self.headers['key'] = key
         super().__init__(wait=wait, retry=retry, system_prompt=system_prompt, verbose=verbose, **kwargs)
 
     def image_to_base64(self, image_path):
@@ -38,8 +36,6 @@ class BailingMMWrapper(BaseAPI):
 
     def prepare_inputs(self, inputs):
         msgs = cp.deepcopy(inputs)
-        content = []
-        text = ''
         content = []
         for i, msg in enumerate(msgs):
             if msg['type'] == 'text':
@@ -53,38 +49,30 @@ class BailingMMWrapper(BaseAPI):
                     image_data = ''
                 msg['value'] = image_data
             content.append(msg)
-        return json.dumps(content, ensure_ascii=False)
+        return content
 
     def generate_inner(self, inputs, **kwargs) -> str:
         assert isinstance(inputs, str) or isinstance(inputs, list)
+        start = time.time()
         inputs = [inputs] if isinstance(inputs, str) else inputs
 
         messages = self.prepare_inputs(inputs)
 
-        service_url = 'http://cv.gateway.alipay.com/ua/invoke'
-        # service_url = 'http://gateway-cv-pre.alipay.com/ua/invoke'
+        service_url = "https://bailingchat.alipay.com/api/proxy/eval/antgmm/completions"
 
         payload = {
-            "serviceCode": "OC_TEST",
-            "uri": "V2",
-            "appId": "test",
-            "appName": "test",
-            "attributes": {
-                "_ROUTE_": "MAYA",
-                "_TIMEOUT_": "60000",
-            },
-            "params": {
-                "objectFeatures": {
-                    "struct_input": messages
-                }
-            },
+            "structInput": messages,
+            "appToken": self.key,
+            "timeout": 180000
         }
-
-        response = requests.post(service_url, headers=self.headers, data=json.dumps(payload), verify=False)
+        response = requests.post(service_url, headers=self.headers, json=payload)
+        if self.verbose:
+            self.logger.info('Time for requesting is:')
+            self.logger.info(time.time() - start)
         try:
             assert response.status_code == 200
             output = json.loads(response.text)
-            answer = output['resultMap']['attributes']['pred']
+            answer = output['preds']['pred']
             if self.verbose:
                 self.logger.info(f'inputs: {inputs}\nanswer: {answer}')
             return 0, answer, 'Succeeded! '
