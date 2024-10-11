058f6733a82954fd4c8c72dfc22c6b3881c93638
Xinyu Fang <fangxinyutju202009@126.com>
2024-10-11 14:48:50 +0800

    [Model] add support for Molmo-7B-O/D-0924 (#513)
    * add support for molmo
    
    * update lint.yml
    
    * fix lint in molmo
    
    ---------
    
    Co-authored-by: Haodong Duan <dhd@pku.edu.cn>

--- vlmeval/vlm/__init__.py
+++ vlmeval/vlm/__init__.py
@@ -50,4 +50,5 @@ from .slime import SliME
 from .mplug_owl3 import mPLUG_Owl3
 from .pixtral import Pixtral
 from .llama_vision import llama_vision
+from .molmo import molmo
 from .points import POINTS
