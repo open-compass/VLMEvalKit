d1c6d67d00aa1ad1df62464970cc77ad25722094
kennymckormick <dhd.efz@gmail.com>
2024-10-11 14:18:14 +0800

    remove duplicate

--- vlmeval/dataset/image_mcq.py
+++ vlmeval/dataset/image_mcq.py
@@ -305,98 +305,6 @@ class MMMUDataset(ImageMCQDataset):
         return msgs
 
 
-class GMAIMMBenchDataset(ImageMCQDataset):
-
-    DATASET_URL = {
-        'GMAI_mm_bench_VAL': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/blob/main/GMAI_mm_bench_VAL.tsv'
-    }
-
-    DATASET_MD5 = {
-        'GMAI_mm_bench_VAL': '254bd581627866f1c499d3d6b4422324'
-    }
-
-    def report_acc_by_groups(self, df, group_column):
-        res = defaultdict(list)
-
-        # Check for the 'split' column
-        if 'split' in df:
-            splits = list(set(df['split']))
-            res['split'] = splits
-        else:
-            df['split'] = ['none'] * len(df)
-            res['split'] = ['none']
-
-        res['Overall'] = [np.mean(df[df['split'] == sp]['hit']) for sp in res['split']]
-
-        if group_column not in df:
-            raise ValueError(f"Column '{group_column}' not found in dataframe.")
-
-        abilities = list(set(df[group_column]))
-        abilities = ['None' if isinstance(ab, float) and pd.isna(ab) else ab for ab in abilities]
-        abilities.sort()
-
-        for ab in abilities:
-            ab_name = ab
-            sub_df = df[df[group_column] == ab]
-            res[ab_name] = [np.mean(sub_df[sub_df['split'] == sp]['hit']) for sp in res['split']]
-
-        return pd.DataFrame(res)
-
-    def evaluate(self, eval_file, **judge_kwargs):
-        from .utils.multiple_choice import report_acc, mcq_vanilla_eval
-        nproc = judge_kwargs.pop('nproc', 4)
-
-        suffix = eval_file.split('.')[-1]
-        model = judge_kwargs.get('model', 'exact_matching')
-        assert model in ['chatgpt-0125', 'exact_matching', 'gpt-4-0125']
-        name_str_map = {'chatgpt-0125': 'openai', 'gpt-4-0125': 'gpt4'}
-        name_str = name_str_map[model] if model in name_str_map else model
-
-        if model == 'exact_matching':
-            model = None
-        elif gpt_key_set():
-            model = build_judge(**judge_kwargs)
-            if not model.working():
-                warnings.warn('OPENAI API is not working properly, will use exact matching for evaluation')
-                warnings.warn(DEBUG_MESSAGE)
-                model = None
-        else:
-            warnings.warn('OPENAI_API_KEY is not set properly, will use exact matching for evaluation')
-            model = None
-
-        result_file = eval_file.replace(f'.{suffix}', f'_{name_str}_result.pkl')
-
-        data = load(eval_file)
-        data = data.sort_values(by='index')
-        data['prediction'] = [str(x) for x in data['prediction']]
-        # If not choice label, then use lower case
-        for k in data.keys():
-            data[k.lower() if k not in list(string.ascii_uppercase) else k] = data.pop(k)
-
-        meta = self.data
-        meta_q_map = {x: y for x, y in zip(meta['index'], meta['question'])}
-        data_map = {x: y for x, y in zip(data['index'], data['question'])}
-        for k in data_map:
-            assert k in meta_q_map, (
-                f'eval_file should be the same as or a subset of dataset {self.dataset_name}'
-            )
-
-        data = mcq_vanilla_eval(model, data, meta, nproc, result_file, self.dataset_name)
-
-        # load split
-        dump(data, eval_file.replace(f'.{suffix}', f'_{name_str}_result.{suffix}'))
-        data = load(eval_file.replace(f'.{suffix}', f'_{name_str}_result.{suffix}'))
-
-        acc = report_acc(data)
-
-        for group_col in ['clinical vqa task', 'department', 'perceptual granularity']:
-            acc_grouped = self.report_acc_by_groups(data, group_col)
-            score_file_grouped = eval_file.replace(f'.{suffix}', f'_{group_col}_acc.csv')
-            dump(acc_grouped, score_file_grouped)
-
-        return acc
-
-
 class MUIRDataset(ImageMCQDataset):
 
     DATASET_URL = {
@@ -479,17 +387,17 @@ class GMAIMMBenchDataset(ImageMCQDataset):
 
     DATASET_URL = {
         'GMAI-MMBench_VAL': 'https://huggingface.co/datasets/VLMEval/GMAI-MMBench/resolve/main/GMAI-MMBench_VAL.tsv',
-        'GMAI_mm_bench_TEST_part_1': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_1.tsv',
-        'GMAI_mm_bench_TEST_part_2': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_2.tsv',
-        'GMAI_mm_bench_TEST_part_3': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_3.tsv',
-        'GMAI_mm_bench_TEST_part_4': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_4.tsv',
-        'GMAI_mm_bench_TEST_part_5': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_5.tsv',
-        'GMAI_mm_bench_TEST_part_6': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_6.tsv',
-        'GMAI_mm_bench_TEST_part_7': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_7.tsv',
-        'GMAI_mm_bench_TEST_part_8': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_8.tsv',
-        'GMAI_mm_bench_TEST_part_9': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_9.tsv',
-        'GMAI_mm_bench_TEST_part_10': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_10.tsv',
-        'GMAI_mm_bench_TEST_part_11': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_11.tsv',
+        'GMAI_mm_bench_TEST_part_1': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_1.tsv',  # noqa: E501
+        'GMAI_mm_bench_TEST_part_2': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_2.tsv',  # noqa: E501
+        'GMAI_mm_bench_TEST_part_3': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_3.tsv',  # noqa: E501
+        'GMAI_mm_bench_TEST_part_4': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_4.tsv',  # noqa: E501
+        'GMAI_mm_bench_TEST_part_5': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_5.tsv',  # noqa: E501
+        'GMAI_mm_bench_TEST_part_6': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_6.tsv',  # noqa: E501
+        'GMAI_mm_bench_TEST_part_7': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_7.tsv',  # noqa: E501
+        'GMAI_mm_bench_TEST_part_8': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_8.tsv',  # noqa: E501
+        'GMAI_mm_bench_TEST_part_9': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_9.tsv',  # noqa: E501
+        'GMAI_mm_bench_TEST_part_10': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_10.tsv',  # noqa: E501
+        'GMAI_mm_bench_TEST_part_11': 'https://huggingface.co/datasets/OpenGVLab/GMAI-MMBench/resolve/main/GMAI_mm_bench_TEST_part_11.tsv',  # noqa: E501
     }
 
     DATASET_MD5 = {
@@ -558,7 +466,7 @@ class GMAIMMBenchDataset(ImageMCQDataset):
         res['Overall'] = [np.mean(df[df['split'] == sp]['hit']) for sp in res['split']]
 
         if group_column not in df:
-            raise ValueError(f"Column '{group_column}' not found in dataframe.")
+            raise ValueError(f"Column '{group_column}' not found in dataframe.")  # noqa: E713
 
         abilities = list(set(df[group_column]))
         abilities = ['None' if isinstance(ab, float) and pd.isna(ab) else ab for ab in abilities]
