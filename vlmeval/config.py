1132a67a9161f1dc52fb19b83b7002e2341047ab
Xinyu Fang <fangxinyutju202009@126.com>
2024-10-11 14:48:50 +0800

    [Model] add support for Molmo-7B-O/D-0924 (#513)
    * add support for molmo
    
    * update lint.yml
    
    * fix lint in molmo
    
    ---------
    
    Co-authored-by: Haodong Duan <dhd@pku.edu.cn>

--- vlmeval/config.py
+++ vlmeval/config.py
@@ -292,6 +292,11 @@ llama_series={
     'Llama-3.2-90B-Vision-Instruct': partial(llama_vision, model_path='meta-llama/Llama-3.2-90B-Vision-Instruct'),
 }
 
+molmo_series={
+    'molmo-7B-D-0924': partial(molmo, model_path='allenai/Molmo-7B-D-0924'),
+    'molmo-7B-O-0924': partial(molmo, model_path='allenai/Molmo-7B-O-0924'),
+}
+
 kosmos_series={
     'Kosmos2': partial(Kosmos2, model_path='/root/kosmos-2-patch14-224')
 }
@@ -309,8 +314,8 @@ model_groups = [
     deepseekvl_series, minicpm_series, cogvlm_series, wemm_series,
     cambrian_series, chameleon_series, video_models, ovis_series, vila_series,
     mantis_series, mmalaya_series, phi3_series, xgen_mm_series, qwen2vl_series, 
-    slime_series, eagle_series, moondream_series, llama_series, kosmos_series,
-    points_series
+    slime_series, eagle_series, moondream_series, llama_series, molmo_series, 
+    kosmos_series, points_series
 ]
 
 for grp in model_groups:
