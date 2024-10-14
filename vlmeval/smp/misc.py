970f34557566cbececd3267637cee8ed14a7cf33
kennymckormick <dhd.efz@gmail.com>
2024-10-14 21:58:25 +0800

    fix bug

--- vlmeval/smp/misc.py
+++ vlmeval/smp/misc.py
@@ -159,13 +159,13 @@ def load_env():
     try:
         import vlmeval
     except ImportError:
-        logger.error('VLMEval is not installed. Failed to import environment variables from .env file. ')
+        logging.error('VLMEval is not installed. Failed to import environment variables from .env file. ')
         return
     pth = osp.realpath(vlmeval.__path__[0])
     pth = osp.join(pth, '../.env')
     pth = osp.realpath(pth)
     if not osp.exists(pth):
-        logger.error(f'Did not detect the .env file at {pth}, failed to load. ')
+        logging.error(f'Did not detect the .env file at {pth}, failed to load. ')
         return
 
     from dotenv import dotenv_values
@@ -173,7 +173,7 @@ def load_env():
     for k, v in values.items():
         if v is not None and len(v):
             os.environ[k] = v
-    logger.info(f'API Keys successfully loaded from {pth}')
+    print(f'API Keys successfully loaded from {pth}')
 
 def pip_install_robust(package):
     import sys
