eb3dbf1a8e0cb862ea04ebd7fa686a7a90ab3551
kennymckormick <dhd.efz@gmail.com>
2024-10-25 17:44:47 +0800

    add claude3.5v sonnet 20241022

--- vlmeval/config.py
+++ vlmeval/config.py
@@ -80,6 +80,7 @@ api_models = {
     'Claude3V_Sonnet': partial(Claude3V, model='claude-3-sonnet-20240229', temperature=0, retry=10),
     'Claude3V_Haiku': partial(Claude3V, model='claude-3-haiku-20240307', temperature=0, retry=10),
     'Claude3-5V_Sonnet': partial(Claude3V, model='claude-3-5-sonnet-20240620', temperature=0, retry=10),
+    'Claude3-5V_Sonnet_20241022': partial(Claude3V, model='claude-3-5-sonnet-20241022', temperature=0, retry=10),
     # GLM4V
     'GLM4V': partial(GLMVisionAPI, model='glm4v-biz-eval', temperature=0, retry=10),
     # CongRong
