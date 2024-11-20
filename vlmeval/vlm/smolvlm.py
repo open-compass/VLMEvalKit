babe0537655728d138bd2d4d9049ab28750165a2
Miquel Farre <miquel.farre@huggingface.co>
2024-11-20 18:59:18 +0000

    pre-commit fixes

--- vlmeval/vlm/smolvlm.py
+++ vlmeval/vlm/smolvlm.py
@@ -10,20 +10,21 @@ from transformers import AutoProcessor, Idefics3ForConditionalGeneration
 import os
 import math
 
+
 class SmolVLM(BaseModel):
     INSTALL_REQ = True
     INTERLEAVE = True
 
     def __init__(self, model_path='HuggingFaceTB/SmolVLM-Instruct', **kwargs):
         assert osp.exists(model_path) or splitlen(model_path) == 2
-        
+
         self.processor = AutoProcessor.from_pretrained(model_path)
         self.model = Idefics3ForConditionalGeneration.from_pretrained(
             model_path,
             torch_dtype=torch.float32,
             device_map='cuda'
         )
-        
+
         kwargs_default = {'max_new_tokens': 512,
                           'use_cache': True}
         kwargs_default.update(kwargs)
@@ -31,16 +32,16 @@ class SmolVLM(BaseModel):
         warnings.warn(f'Following kwargs received: {self.kwargs}, will use as generation config.')
         torch.cuda.empty_cache()
 
-
     def generate_inner(self, message, dataset=None):
-        if dataset in ['MMBench_DEV_EN', 'MMBench_TEST_EN', 'MMBench_DEV_CN', 'MMBench_TEST_CN', 'MMBench', 'MMBench_CN']:
+        if dataset in ['MMBench_DEV_EN', 'MMBench_TEST_EN', 'MMBench_DEV_CN', 'MMBench_TEST_CN', 'MMBench',
+                       'MMBench_CN']:
             formatted_messages, formatted_images = self.build_prompt_mmbench(message)
         elif dataset in ['MMMU_DEV_VAL', 'MMMU_TEST']:
             formatted_messages, formatted_images = self.build_prompt_mmmu(message)
         elif dataset in ['MathVista_MINI']:
             formatted_messages, formatted_images = self.build_prompt_mathvista(message)
-        elif dataset in ['MME', 'MMVet', 'OCRVQA_TEST', 'OCRVQA_TESTCORE', 'TextVQA_VAL', 'ChartQA_TEST',
-                        'DocVQA_VAL', 'DocVQA_TEST', 'InfoVQA_VAL', 'InfoVQA_TEST']:
+        elif dataset in ['MME', 'MMVet', 'OCRVQA_TEST', 'OCRVQA_TESTCORE', 'TextVQA_VAL',
+                         'ChartQA_TEST', 'DocVQA_VAL', 'DocVQA_TEST', 'InfoVQA_VAL', 'InfoVQA_TEST']:
             formatted_messages, formatted_images = self.build_prompt_default(message, add_brief=True)
         elif dataset == 'HallusionBench':
             formatted_messages, formatted_images = self.build_prompt_default(message, add_yes_or_no=True)
@@ -48,22 +49,17 @@ class SmolVLM(BaseModel):
             formatted_messages, formatted_images = self.build_prompt_puremcq(message)
         else:
             formatted_messages, formatted_images = self.build_prompt_default(message)
-            
+
         images = [formatted_images] if isinstance(formatted_images, Image.Image) else formatted_images
-        
-        resulting_messages = [{"role": "user", "content": [{"type": "image"}] + [{"type": "text", "text": formatted_messages}]}]
-        # image_contents = [{"type": "image"} for _ in range(len(images))]
-        # resulting_messages = [{"role": "user", "content": image_contents + [{"type": "text", "text": formatted_messages}]}]
-        
         inputs = self.processor(text=formatted_messages, images=images, return_tensors="pt")
         inputs = {k: v.to(self.model.device) for k, v in inputs.items()}
 
         generated_ids = self.model.generate(**inputs, **self.kwargs)
         generated_text = self.processor.batch_decode(
-            generated_ids[:, inputs['input_ids'].size(1):], 
+            generated_ids[:, inputs['input_ids'].size(1):],
             skip_special_tokens=True
         )[0]
-        
+
         return generated_text.strip()
 
     def build_prompt_default(self, message, add_brief=False, add_yes_or_no=False):
@@ -206,14 +202,14 @@ class SmolVLM(BaseModel):
             prompt += ' Answer:'
         return prompt, images
 
-
     def chat_inner(self, message, dataset=None):
         formatted_messages, formatted_images = self.build_prompt_mt(message)
         images = [formatted_images] if isinstance(formatted_images, Image.Image) else formatted_images
-        
-        resulting_messages = [{"role": "user", "content": [{"type": "image"}] + [{"type": "text", "text": formatted_messages}]}]
+
+        resulting_messages = [{"role": "user", "content": [{"type": "image"}]
+                               + [{"type": "text", "text": formatted_messages}]}]
         prompt = self.processor.apply_chat_template(resulting_messages, add_generation_prompt=True)
-        
+
         inputs = self.processor(text=prompt, images=images, return_tensors="pt")
         inputs = {k: v.to(self.model.device) for k, v in inputs.items()}
 
@@ -222,5 +218,5 @@ class SmolVLM(BaseModel):
             generated_ids[:, inputs['input_ids'].size(1):],
             skip_special_tokens=True
         )[0]
-        
+
         return generated_text.strip()
