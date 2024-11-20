f654011890671b2d4a25c6b87f49bedfaddb12ea
zsoltzsolt <z.diriczi@yahoo.com>
2024-11-20 19:58:47 +0000

    add llava one vision hf format models

--- vlmeval/vlm/__init__.py
+++ vlmeval/vlm/__init__.py
@@ -10,7 +10,7 @@ from .eagle_x import Eagle
 from .idefics import IDEFICS, IDEFICS2
 from .instructblip import InstructBLIP
 from .kosmos import Kosmos2
-from .llava import LLaVA, LLaVA_Next, LLaVA_XTuner, LLaVA_Next2, LLaVA_OneVision
+from .llava import LLaVA, LLaVA_Next, LLaVA_XTuner, LLaVA_Next2, LLaVA_OneVision, LLaVA_OneVision_HF
 from .minicpm_v import MiniCPM_V, MiniCPM_Llama3_V, MiniCPM_V_2_6
 from .minigpt4 import MiniGPT4
 from .mmalaya import MMAlaya, MMAlaya2
