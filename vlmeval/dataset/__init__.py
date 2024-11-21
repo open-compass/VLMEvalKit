cfacfdd8402469fadcbacca79d48cc38dd4287fe
Andrew <andrewliao11@gmail.com>
2024-11-20 22:09:37 -0500

    [Benchmark] Measuring Quantitative Spatial Reasoning with the Q-Spatial Bench🔥 (#569)
    * add Q-Spatial Bench
    
    * perform pre-commit
    
    * use spatialprompt_single
    
    ---------
    
    Co-authored-by: Haodong Duan <dhd@pku.edu.cn>

--- vlmeval/dataset/__init__.py
+++ vlmeval/dataset/__init__.py
@@ -10,7 +10,7 @@ from .image_mcq import (
 from .image_mt import MMDUDataset
 from .image_vqa import (
     ImageVQADataset, MathVision, OCRBench, MathVista, LLaVABench, MMVet, MTVQADataset, TableVQABench,
-    CustomVQADataset, CRPE, MathVerse, OlympiadBench
+    CustomVQADataset, CRPE, MathVerse, OlympiadBench, QSpatial
 )
 
 from .text_mcq import CustomTextMCQDataset, TextMCQDataset
@@ -126,7 +126,7 @@ IMAGE_DATASET = [
     MMMUDataset, OCRBench, MathVista, LLaVABench, MMVet, MTVQADataset, TableVQABench,
     MMLongBench, VCRDataset, MMDUDataset, DUDE, SlideVQA, MUIRDataset,
     GMAIMMBenchDataset, MMERealWorld, HRBenchDataset, CRPE, MathVerse, NaturalBenchDataset,
-    MIABench, OlympiadBench, WildVision, MMMath
+    MIABench, OlympiadBench, WildVision, MMMath, QSpatial
 ]
 
 VIDEO_DATASET = [
