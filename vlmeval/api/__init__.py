1d243f9c9504bb02f65b1655f6a837f4766245eb
Haodong Duan <dhd@pku.edu.cn>
2024-11-27 17:10:37 +0800

    [Model] Support SiliconFlowAPI and add two more SmolVLM variants (#631)

--- vlmeval/api/__init__.py
+++ vlmeval/api/__init__.py
@@ -8,12 +8,12 @@ from .reka import Reka
 from .glm_vision import GLMVisionAPI
 from .cloudwalk import CWWrapper
 from .sensechat_vision import SenseChatVisionAPI
+from .siliconflow import SiliconFlowAPI, TeleMMAPI
 from .hunyuan import HunyuanVision
 from .bailingmm import bailingMMAPI
 from .bluelm_v_api import BlueLMWrapper, BlueLM_V_API
 from .jt_vl_chat import JTVLChatAPI
 from .taiyi import TaiyiAPI
-from .telemm import TeleMM
 
 
 __all__ = [
@@ -22,5 +22,5 @@ __all__ = [
     'Claude3V', 'Claude_Wrapper', 'Reka', 'GLMVisionAPI',
     'CWWrapper', 'SenseChatVisionAPI', 'HunyuanVision', 'Qwen2VLAPI',
     'BlueLMWrapper', 'BlueLM_V_API', 'JTVLChatAPI', 'bailingMMAPI',
-    'TaiyiAPI', 'TeleMM'
+    'TaiyiAPI', 'TeleMMAPI', 'SiliconFlowAPI'
 ]
