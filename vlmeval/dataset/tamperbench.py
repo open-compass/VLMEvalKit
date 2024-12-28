8b6c816596e57f25c9805b306a2e151a02de2c50
Srikant Panda <srikant86.panda@gmail.com>
2024-12-28 14:32:31 +0000

    updated md5 and dataset support

--- vlmeval/dataset/tamperbench.py
+++ vlmeval/dataset/tamperbench.py
@@ -20,7 +20,9 @@ FAIL_MSG = 'Failed to obtain answer via API.'
 class MVTamperBench(VideoBaseDataset):
 
     MD5 = {
-        'MVTamperBench': '12a8bac1b4452c5c7974c31f8e76e882',
+        'MVTamperBench': '3557260881ba47db8add440c5edb742a',
+        'MVTamperBenchStart': '3557260881ba47db8add440c5edb742a',
+        'MVTamperBenchEnd': '3557260881ba47db8add440c5edb742a',
         'MVTamperBenchSample': 'af0059e5e0f894aa61753b5a09c37fc6',
         'MVTamperBenchSample_Start': '69f8ce9be14c297c1e7478b49bbe80db',
         'MVTamperBenchSample_End': 'fb12bf5e0355b8ff6a1b029b2970347c',
@@ -79,7 +81,7 @@ Based on your observations, select the best option that accurately addresses the
 
     @classmethod
     def supported_datasets(cls):
-        return ['MVTamperBench', 'MVTamperBenchSample', 'MVTamperBenchSample_Start', 'MVTamperBenchSample_End']
+        return ['MVTamperBench', 'MVTamperBenchStart', 'MVTamperBenchEnd', 'MVTamperBenchSample', 'MVTamperBenchSample_Start', 'MVTamperBenchSample_End']
 
     def prepare_dataset(self, dataset_name='MVTamperBenchSample', repo_id=None):
         if repo_id:
