5aa51ec2956976dc02f469341a4546ecaa5612ec
kennymckormick <dhd.efz@gmail.com>
2025-01-23 07:19:05 +0000

    update emu

--- vlmeval/vlm/emu.py
+++ vlmeval/vlm/emu.py
@@ -1,11 +1,23 @@
 import os
+import sys
 import torch
 from PIL import Image
 import os.path as osp
 from .base import BaseModel
 from ..smp import *
 from huggingface_hub import snapshot_download
-from PIL import Image
+
+
+def get_local_root(repo_id):
+    if osp.exists(repo_id) and osp.isdir(repo_id):
+        return repo_id
+
+    cache_path = get_cache_path(repo_id, repo_type='models')
+    if cache_path is None:
+        cache_path = snapshot_download(repo_id=repo_id)
+    assert osp.exists(cache_path) and osp.isdir(cache_path)
+    return cache_path
+
 
 class Emu(BaseModel):
 
@@ -89,33 +101,35 @@ class Emu(BaseModel):
         output_text = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
         return output_text[0]
 
+
 class Emu3_chat(BaseModel):
     INSTALL_REQ = True
     INTERLEAVE = False
 
-    def __init__(self, model_path='BAAI/Emu3-Chat', tokenizer_path='BAAI/Emu3-VisionTokenizer', dir_root=None, cuda="cuda", **kwargs):
+    def __init__(self, model_path='BAAI/Emu3-Chat', tokenizer_path='BAAI/Emu3-VisionTokenizer', **kwargs):
         assert model_path is not None
         assert tokenizer_path is not None
-        sys.path.append(dir_root)
         try:
             from transformers import AutoTokenizer, AutoModel, AutoImageProcessor, AutoModelForCausalLM
-            import torch
-            #set emu3 project path
-            from emu3.mllm.processing_emu3 import Emu3Processor
+            local_root = get_local_root(model_path)
+            sys.path.append(local_root)
+            from processing_emu3 import Emu3Processor
         except Exception as err:
-            logging.critical(
-                'Please first install requirements and set the root path to use Emu3—chat. \
-                Follow the instructions at https://github.com/baaivision/Emu3.'
-            )
             raise err
-        #download model wights in huggingface
-        model_path = snapshot_download(model_path)
-        tokenizer_path = snapshot_download(tokenizer_path)
-        #load model wights
-        self.model = model = AutoModelForCausalLM.from_pretrained(model_path, device_map=cuda, torch_dtype=torch.bfloat16,  attn_implementation="flash_attention_2", trust_remote_code=True,)
+
+        # load model wights
+        model = AutoModelForCausalLM.from_pretrained(
+            model_path,
+            device_map='cuda',
+            torch_dtype=torch.bfloat16,
+            attn_implementation="flash_attention_2",
+            trust_remote_code=True)
+        model.eval()
+        self.model = model
         self.tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True, padding_side="left")
         self.image_processor = AutoImageProcessor.from_pretrained(tokenizer_path, trust_remote_code=True)
-        self.image_tokenizer =  AutoModel.from_pretrained(tokenizer_path, device_map=cuda, trust_remote_code=True).eval()
+        self.image_tokenizer = AutoModel.from_pretrained(
+            tokenizer_path, device_map='cuda', trust_remote_code=True).eval()
         self.processor = Emu3Processor(self.image_processor, self.image_tokenizer, self.tokenizer)
         self.kwargs = kwargs
         self.cuda = cuda
@@ -153,50 +167,60 @@ class Emu3_chat(BaseModel):
         outputs = outputs[:, inputs.input_ids.shape[-1]:]
         response = self.processor.batch_decode(outputs, skip_special_tokens=True)[0]
         return response
-    
+
+
 class Emu3_gen(BaseModel):
     INSTALL_REQ = True
     INTERLEAVE = False
 
-    def __init__(self, model_path='BAAI/Emu3-Gen', tokenizer_path='BAAI/Emu3-VisionTokenizer', dir_root=None, cuda="cuda",output_path='', **kwargs):
+    def __init__(self,
+                 model_path='BAAI/Emu3-Gen',
+                 tokenizer_path='BAAI/Emu3-VisionTokenizer',
+                 output_path='',
+                 **kwargs):
+
         assert model_path is not None
         assert tokenizer_path is not None
-        sys.path.append(dir_root)
         try:
             from transformers import AutoTokenizer, AutoModel, AutoImageProcessor, AutoModelForCausalLM
-            import torch
-            #set emu3 project path
-            from emu3.mllm.processing_emu3 import Emu3Processor
+            local_root = get_local_root(model_path)
+            sys.path.append(local_root)
+            from processing_emu3 import Emu3Processor
         except Exception as err:
-            logging.critical(
-                'Please first install requirements and set the root path to use Emu3—chat. \
-                Follow the instructions at https://github.com/baaivision/Emu3.'
-            )
             raise err
-        #download model wights in huggingface
-        model_path = snapshot_download(model_path)
-        tokenizer_path = snapshot_download(tokenizer_path)
-        #load model wights
-        self.model = model = AutoModelForCausalLM.from_pretrained(model_path, device_map=cuda, torch_dtype=torch.bfloat16,  attn_implementation="flash_attention_2", trust_remote_code=True,)
+
+        # load model wights
+        model = AutoModelForCausalLM.from_pretrained(
+            model_path,
+            device_map='cuda',
+            torch_dtype=torch.bfloat16,
+            attn_implementation="flash_attention_2",
+            trust_remote_code=True)
+        model.eval()
+        self.model = model
+
         self.tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True, padding_side="left")
         self.image_processor = AutoImageProcessor.from_pretrained(tokenizer_path, trust_remote_code=True)
-        self.image_tokenizer =  AutoModel.from_pretrained(tokenizer_path, device_map=cuda, trust_remote_code=True).eval()
+        self.image_tokenizer = AutoModel.from_pretrained(
+            tokenizer_path,
+            device_map='cuda',
+            trust_remote_code=True).eval()
         self.processor = Emu3Processor(self.image_processor, self.image_tokenizer, self.tokenizer)
         self.kwargs = kwargs
         self.cuda = cuda
         self.output_path = output_path
 
     def generate_inner(self, message, dataset=None):
-        query, images = '', []
+        query = ''
         for item in message:
             if item['type'] == 'text':
                 query += item['value']
             else:
                 raise ValueError('Please input the text in generation stage.')
-            
+
         # prepare input
         POSITIVE_PROMPT = " masterpiece, film grained, best quality."
-        NEGATIVE_PROMPT = "lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry."
+        NEGATIVE_PROMPT = "lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry."  # noqa: E501
 
         classifier_free_guidance = 3.0
         prompt = "a portrait of young girl."
@@ -205,18 +229,18 @@ class Emu3_gen(BaseModel):
         kwargs = dict(
             mode='G',
             ratio="1:1",
-            image_area= self.model.config.image_area,
+            image_area=self.model.config.image_area,
             return_tensors="pt",
-            padding="longest",
-        )
+            padding="longest")  # noqa: E501
+
         pos_inputs = self.processor(text=prompt, **kwargs)
         neg_inputs = self.processor(text=NEGATIVE_PROMPT, **kwargs)
         from transformers.generation.configuration_utils import GenerationConfig
         # prepare hyper parameters
         GENERATION_CONFIG = GenerationConfig(
             use_cache=True,
-            eos_token_id= self.model.config.eos_token_id,
-            pad_token_id= self.model.config.pad_token_id,
+            eos_token_id=self.model.config.eos_token_id,
+            pad_token_id=self.model.config.pad_token_id,
             max_new_tokens=40960,
             do_sample=True,
             top_k=2048,
@@ -225,17 +249,14 @@ class Emu3_gen(BaseModel):
         h = pos_inputs.image_size[:, 0]
         w = pos_inputs.image_size[:, 1]
         constrained_fn = self.processor.build_prefix_constrained_fn(h, w)
-        from transformers.generation import LogitsProcessorList, PrefixConstrainedLogitsProcessor, UnbatchedClassifierFreeGuidanceLogitsProcessor
+        from transformers.generation import LogitsProcessorList, PrefixConstrainedLogitsProcessor, UnbatchedClassifierFreeGuidanceLogitsProcessor  # noqa: E501
         logits_processor = LogitsProcessorList([
             UnbatchedClassifierFreeGuidanceLogitsProcessor(
                 classifier_free_guidance,
                 self.model,
                 unconditional_ids=neg_inputs.input_ids.to("cuda:0"),
             ),
-            PrefixConstrainedLogitsProcessor(
-                constrained_fn ,
-                num_beams=1,
-            ),
+            PrefixConstrainedLogitsProcessor(constrained_fn, num_beams=1),
         ])
 
         # generate
