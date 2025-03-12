5cf23a0d2b9b9501c8f8d55c239e3495848d0030
EnxinSong <77435739+Espere-1119-Song@users.noreply.github.com>
2025-03-12 15:55:11 +0800

    Update __init__.py

--- vlmeval/vlm/__init__.py
+++ vlmeval/vlm/__init__.py
@@ -3,7 +3,6 @@ import torch
 torch.set_grad_enabled(False)
 torch.manual_seed(1234)
 from .aria import Aria
-from .auroracap import AuroraCap
 from .base import BaseModel
 from .cogvlm import CogVlm, GLM4v
 from .emu import Emu
@@ -43,7 +42,6 @@ from .wemm import WeMM
 from .cambrian import Cambrian
 from .chameleon import Chameleon
 from .video_llm import VideoLLaVA, VideoLLaVA_HF, Chatunivi, VideoChatGPT, LLaMAVID, VideoChat2_HD, PLLaVA
-from .video_llm.moviechat_videollama import MovieChat
 from .vila import VILA
 from .ovis import Ovis, Ovis1_6, Ovis1_6_Plus
 from .mantis import Mantis
