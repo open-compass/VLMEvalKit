2d55e01c4be5043b51ca49921ab3f2ec7efb82ed
chenjix <3107760494@qq.com>
2025-04-08 07:06:17 +0000

    add llama4

--- vlmeval/config.py
+++ vlmeval/config.py
@@ -1048,6 +1048,9 @@ llama_series = {
     "Llama-3.2-90B-Vision-Instruct": partial(
         llama_vision, model_path="meta-llama/Llama-3.2-90B-Vision-Instruct"
     ),
+    "Llama-4-Scout-17B-16E-Instruct": partial(
+        llama_vision, model_path="meta-llama/Llama-4-Scout-17B-16E-Instruct"
+    ),
 }
 
 molmo_series = {
