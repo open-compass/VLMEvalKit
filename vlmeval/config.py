011acbd4060b67f3452da56b2a49b5a743ba64e8
TianhaoLiang2000 <133112250+TianhaoLiang2000@users.noreply.github.com>
2025-04-11 21:18:29 +0800

    [Model] Add Kimi-VL-A3B-Thinking and Kimi-VL-A3B-Instruct (#904)
    * add Llama4
    
    * add Llama4 file config
    
    * add vLLM version of Llama 4
    
    * add vLLM version of Llama 4
    
    * fix import of vllm
    
    * [Model] Add Kimi-VL-A3B-Thinking and Kimi-VL-A3B-Instruct
    
    * Clean print code
    
    ---------
    
    Co-authored-by: TianhaoLiang2000 <tianhao.liang2000@gmail.com>
    Co-authored-by: Haodong Duan <dhd@pku.edu.cn>

--- vlmeval/config.py
+++ vlmeval/config.py
@@ -1205,6 +1205,11 @@ gemma_series = {
     'Gemma3-27B': partial(Gemma3, model_path='google/gemma-3-27b-it')
 }
 
+kimi_series = {
+    'Kimi-VL-A3B-Thinking': partial(KimiVL, model_path='moonshotai/Kimi-VL-A3B-Thinking'),
+    'Kimi-VL-A3B-Instruct': partial(KimiVL, model_path='moonshotai/Kimi-VL-A3B-Instruct')
+}
+
 internvl_groups = [
     internvl, internvl2, internvl2_5, mini_internvl, internvl2_5_mpo, 
     internvl3,
@@ -1226,7 +1231,7 @@ model_groups = [
     kosmos_series, points_series, nvlm_series, vintern_series, h2ovl_series,
     aria_series, smolvlm_series, sail_series, valley_series, vita_series,
     ross_series, emu_series, ola_series, ursa_series, gemma_series,
-    long_vita_series, ristretto_series
+    long_vita_series, ristretto_series, kimi_series
 ]
 
 for grp in model_groups:
