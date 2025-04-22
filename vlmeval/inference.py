508c7a0cc4eb94de1386ad05fc1894067042283a
TianhaoLiang2000 <tianhao.liang2000@gmail.com>
2025-04-22 05:38:00 +0000

    [Feature] Add vLLM support for Qwen2-VL/Qwen2.5-VL

--- vlmeval/inference.py
+++ vlmeval/inference.py
@@ -108,7 +108,11 @@ def infer_data(model, model_name, work_dir, dataset, out_file, verbose=False, ap
     lt = len(data)
 
     kwargs = {}
-    if model_name is not None and 'Llama-4' in model_name:
+    if model_name is not None and (
+        'Llama-4' in model_name
+        or 'Qwen2-VL' in model_name
+        or 'Qwen2.5-VL' in model_name
+    ):
         kwargs = {'use_vllm': use_vllm}
     model = supported_VLM[model_name](**kwargs) if isinstance(model, str) else model
 
