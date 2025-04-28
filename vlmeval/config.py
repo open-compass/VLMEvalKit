ff6a34802548029299cf7e6e458cc0ca99a92702
kennymckormick <dhd.efz@gmail.com>
2025-04-28 12:09:34 +0800

    [Model] Support SenseNova-V6

--- vlmeval/config.py
+++ vlmeval/config.py
@@ -359,9 +359,9 @@ api_models = {
     ),
     # CongRong
     "CloudWalk": partial(CWWrapper, model="cw-congrong-v1.5", temperature=0, retry=10),
-    # SenseChat-V
-    "SenseChat-Vision": partial(
-        SenseChatVisionAPI, model="SenseChat-Vision", temperature=0, retry=10
+    # SenseNova
+    "SenseNova-V6": partial(
+        SenseChatVisionAPI, model="SenseNova-V6-Pro-Reasoner", temperature=0, retry=10
     ),
     "HunYuan-Vision": partial(
         HunyuanVision, model="hunyuan-vision", temperature=0, retry=10
