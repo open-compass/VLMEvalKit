41489af9fe2c122ebbd3766965f6b2b4c399b3cd
MaoSong2022 <maosong@pjlab.org.cn>
2025-04-28 11:01:52 +0800

    [Benchmark] Support CVBench (CV-Bench-2D, CV-Bench-3D) (#909)
    * [Benchmark] Support CVBench, including CV-Bench-2D, CV-Bench-3D two sub tasks.
    
    * fix(image_mcq.py): prompt error

--- vlmeval/dataset/__init__.py
+++ vlmeval/dataset/__init__.py
@@ -5,7 +5,7 @@ from .image_caption import ImageCaptionDataset
 from .image_yorn import ImageYORNDataset
 from .image_mcq import (
     ImageMCQDataset, MMMUDataset, CustomMCQDataset, MUIRDataset, GMAIMMBenchDataset, MMERealWorld, HRBenchDataset,
-    NaturalBenchDataset, WeMath, MMMUProDataset, VMCBenchDataset, MedXpertQA_MM_test, LEGO, VisuLogic
+    NaturalBenchDataset, WeMath, MMMUProDataset, VMCBenchDataset, MedXpertQA_MM_test, LEGO, VisuLogic, CVBench
 )
 from .image_mt import MMDUDataset
 from .image_vqa import (
@@ -156,7 +156,7 @@ IMAGE_DATASET = [
     MMNIAH, CMMMU, VLRewardBench, WeMath, LogicVista, MMMUProDataset,
     CreationMMBenchDataset, ImageShortQADataset, MMAlignBench, OmniDocBench,
     VLM2Bench, VMCBenchDataset, EMMADataset, MME_CoT, MOAT, MedXpertQA_MM_test,
-    LEGO, MMSci_Captioning, Physics_yale, MMIFEval, Spatial457, VisuLogic,
+    LEGO, MMSci_Captioning, Physics_yale, MMIFEval, Spatial457, VisuLogic, CVBench
 ]
 
 
