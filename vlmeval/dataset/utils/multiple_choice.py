e2d41d89079f6c9352cd9fd27cbc7cdca77d11be
FangXinyu-0913 <fangxinyutju202009@126.com>
2025-04-29 06:38:44 +0000

    [Fix] update mmmu_evaluation code

--- vlmeval/dataset/utils/multiple_choice.py
+++ vlmeval/dataset/utils/multiple_choice.py
@@ -626,6 +626,80 @@ def get_dimension_rating(data_path):
     return results
 
 
+def extract_numbers(string):
+    """
+    Exact all forms of numbers from a string with regex.
+    """
+    # Pattern for numbers with commas
+    pattern_commas = r'-?\b\d{1,3}(?:,\d{3})+\b'
+    # Pattern for scientific notation
+    pattern_scientific = r'-?\d+(?:\.\d+)?[eE][+-]?\d+'
+    # Pattern for simple numbers without commas
+    pattern_simple = r'-?(?:\d+\.\d+|\.\d+|\d+\b)(?![eE][+-]?\d+)(?![,\d])'
+
+    # Extract numbers with commas
+    numbers_with_commas = re.findall(pattern_commas, string)
+    # Extract numbers in scientific notation
+    numbers_scientific = re.findall(pattern_scientific, string)
+    # Extract simple numbers without commas
+    numbers_simple = re.findall(pattern_simple, string)
+
+    # Combine all extracted numbers
+    all_numbers = numbers_with_commas + numbers_scientific + numbers_simple
+    return all_numbers
+
+
+def parse_open_response(response):
+    """
+    Parse the prediction from the generated response.
+    Return a list of predicted strings or numbers.
+    """
+    # content = content.strip("\n").strip(".").strip(" ")
+    def get_key_subresponses(response):
+        key_responses = []
+        response = response.strip().strip(".").lower()
+        sub_responses = re.split(r'\.\s(?=[A-Z])|\n', response)
+        indicators_of_keys = ['could be ', 'so ', 'is ',
+                            'thus ', 'therefore ', 'final ', 'answer ', 'result ']
+        key_responses = []
+        for index, resp in enumerate(sub_responses):
+            # if last one, accept it's an equation (the entire response can be just one sentence with equation)
+            if index == len(sub_responses) - 1:
+                indicators_of_keys.extend(['='])
+            shortest_key_response = None # the shortest response that may contain the answer (tail part of the response)
+            for indicator in indicators_of_keys:
+                if indicator in resp:
+                    if not shortest_key_response:
+                        shortest_key_response = resp.split(indicator)[-1].strip()
+                    else:
+                        if len(resp.split(indicator)[-1].strip()) < len(shortest_key_response):
+                            shortest_key_response = resp.split(indicator)[-1].strip()
+                    # key_responses.append(resp.split(indicator)[1].strip())
+            
+            if shortest_key_response:
+                # and it's not trivial
+                if shortest_key_response.strip() not in [":", ",", ".", "!", "?", ";", ":", "'"]:
+                    key_responses.append(shortest_key_response)
+        if len(key_responses) == 0: # did not found any
+            return [response]
+        return key_responses
+    # pdb.set_trace()
+    key_responses = get_key_subresponses(response)
+
+    pred_list = key_responses.copy() # keep the original string response
+    for resp in key_responses:
+        pred_list.extend(extract_numbers(resp))
+
+    tmp_pred_list = []
+    for i in range(len(pred_list)):
+        tmp_pred_list.extend(normalize_str(pred_list[i]))
+    pred_list = tmp_pred_list
+
+    # remove duplicates
+    pred_list = list(set(pred_list))
+
+    return pred_list
+
 def check_is_number(string):
     """
     Check if the given string a number.
@@ -674,7 +748,8 @@ def mmmu_evaluation(model, line, dataset_name):
                 norm_answers.extend(normalize_str(answer))
         else:
             norm_answers = normalize_str(line['answer'])
-        for pred in line['prediction']:  # pred is already normalized in parse response phase
+        parsed_pred = parse_open_response(line['prediction'])
+        for pred in parsed_pred:  # pred is already normalized in parse response phase
             if isinstance(pred, str):  # if it's a string, then find if ans in the pred_i
                 for norm_ans in norm_answers:
                     # only see if the string answer in the string pred
@@ -686,7 +761,7 @@ def mmmu_evaluation(model, line, dataset_name):
             else:  # it's a float number
                 if pred in norm_answers:
                     if not hit:
-                        hit = True
+                        hit = 1
                         match_log = 'pred is float, hit the answer'
                     break
         return dict(hit=hit, log=f'Match Log: {match_log}. ')
