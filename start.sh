c9475fa8c17079a4fc209f7c8823d98a1da9eca5
wutaiqiang <2795823986@qq.com>
2025-05-23 16:09:40 +0800

    rm start

--- start.sh
+++
@@ -1,9 +0,0 @@
-# export OPENAI_API_KEY=
-# export LMUData="./LMUData"
-# export SiliconFlow_API_KEY=
-
-
-# valid_type: STR, LLM
-python -u run.py --data PhyX_mini \
-    --model InternVL2_5-8B \
-    --judge deepseek --judge-args '{"valid_type": "STR"}'
