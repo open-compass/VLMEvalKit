86b491a9123b0caad0da58d5f59e129c806ca306
weishi <bushou@xiaohongshu.com>
2025-06-21 18:35:32 +0800

    update

--- run.py
+++ run.py
@@ -349,6 +349,8 @@ def main():
                         dist.barrier()
 
                     dataset = build_dataset(dataset_name, **dataset_kwargs)
+                    dataset.dataset_name = encode_dataset_name(dataset.dataset_name, test_range=test_range)
+                    dataset_name = dataset.dataset_name
                     if dataset is None:
                         raise ValueError( str((dataset_name, dataset_kwargs)) )
                     dataset.dataset_name = encode_dataset_name(dataset.dataset_name, test_range=test_range)
