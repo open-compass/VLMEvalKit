31412b4d19233c09b18e0c8907928282d35d3169
kennymckormick <dhd.efz@gmail.com>
2025-06-21 16:41:41 +0800

    update

--- vlmeval/api/gpt.py
+++ vlmeval/api/gpt.py
@@ -223,7 +223,6 @@ class OpenAIWrapper(BaseAPI):
         response = requests.post(
             self.api_base,
             headers=headers, data=json.dumps(payload), timeout=self.timeout * 1.1)
-        print(response.text)
         ret_code = response.status_code
         ret_code = 0 if (200 <= int(ret_code) < 300) else ret_code
         answer = self.fail_msg
