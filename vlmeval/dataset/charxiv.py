2e937e6a202305e627bfcbc028323dcbaa442345
Li Xiaozhe <148427298+OliverLeeXZ@users.noreply.github.com>
2025-06-30 18:10:51 +0800

    [minor] fix bug in charxiv (#1118)

--- vlmeval/dataset/charxiv.py
+++ vlmeval/dataset/charxiv.py
@@ -227,7 +227,7 @@ class CharXiv(ImageBaseDataset):
 
         # Identify unprocessed indices
         indices = [i for i in range(len(data)) if i not in processed_results]
-        tups = [(judge_model, data.iloc[i]) for i in range(len(data))]
+        tups = [(judge_model, data.iloc[i]) for i in range(len(data)) if i not in processed_results]
 
         # Process remaining examples
         nproc = judge_kwargs.pop("nproc", 4)
