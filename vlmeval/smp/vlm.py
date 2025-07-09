88047e87019ae78bc39a0b5aba18d8b66e1031f3
kennymckormick <dhd.efz@gmail.com>
2025-07-09 14:03:36 +0800

    [Fix] Fix Lint

--- vlmeval/smp/vlm.py
+++ vlmeval/smp/vlm.py
@@ -188,7 +188,7 @@ def read_ok(img_path):
 def gpt_key_set():
     openai_key = os.environ.get('OPENAI_API_KEY', None)
     if openai_key is None:
-        openai_key = os.environ.get('AZURE_OPENAI_API_KEY', None) 
+        openai_key = os.environ.get('AZURE_OPENAI_API_KEY', None)
         return isinstance(openai_key, str)
     return isinstance(openai_key, str) and openai_key.startswith('sk-')
 
