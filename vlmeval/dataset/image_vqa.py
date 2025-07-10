d3c0d5000c6dab17e4fb5eb5ba41fa286c2adbfc
KunXiang <50045221+Quinn777@users.noreply.github.com>
2025-07-11 00:27:18 +0800

    [Benchmark] Support SeePhys benchmark (#1133)
    * [Benchmark] Support SeePhys benchmark
    
    * [Update] Support SeePhys benchmark
    
    * [Minor] Fix bug of md5 value
    
    * [minor] fix lint
    
    * Fix Lint
    
    * Fix Lint
    
    * update requirements
    
    * [Fix] Fix Prompt
    
    * update judge
    
    * [Fix] Fix  Accuracy Bug
    
    ---------
    
    Co-authored-by: kennymckormick <dhd.efz@gmail.com>

--- vlmeval/dataset/image_vqa.py
+++ vlmeval/dataset/image_vqa.py
@@ -866,6 +866,120 @@ class OlympiadBench(ImageBaseDataset):
         return accdz
 
 
+class SeePhys(ImageBaseDataset):
+    TYPE = 'VQA'
+    DATASET_URL = {
+        'SeePhys':
+        'https://huggingface.co/datasets/SeePhys/SeePhys/resolve/main/data_vlmevalkit/SeePhys_total.tsv',
+        'SeePhys_vo':
+        'https://huggingface.co/datasets/SeePhys/SeePhys/resolve/main/data_vlmevalkit/SeePhys_total_vo.tsv',
+    }
+    DATASET_MD5 = {
+        'SeePhys': 'c19612ca99cc8c1351b6c3aa556d25b8',
+        'SeePhys_vo': '542b4e78cbe6b34b247bdbc8aaddeb19',
+    }
+    # Text Vision is the default setting for SeePhys DATASET
+    # Vision Only: Use SeePhys_vo DATASET
+    # Text Caption: Add 'export USE_CAPTION=1' and 'export USE_IMAGE=0' before running the script
+    # Text Only: Add 'export USE_IMAGE=0' before running the script
+
+    def build_prompt(self, line):
+        if isinstance(line, int):
+            line = self.data.iloc[line]
+
+        if self.meta_only:
+            tgt_path = toliststr(line['image_path'])
+        else:
+            tgt_path = self.dump_image(line)
+
+        question = "" if str(line['question']) == 'nan' else line['question']
+
+        if os.environ.get('USE_CAPTION', '0') == '1':
+            question += line['caption']
+
+        if os.environ.get('USE_SEARCH', '0') == '1':
+            if line['language'] == 'English':
+                question += "\nPlease search the Internet to answer the above question. First output your thinking process in <think> </think> tags and then output the final answer in <answer> </answer> tags."  # noqa: E501
+            else:
+                question += "\n请在互联网上搜索以回答上述问题。首先在<think></think>标签中输出你的思维过程，然后在<answer></answer>标签中输入最终答案。"  # noqa: E501
+        elif os.environ.get('USE_COT_PROMPT', '1') == '1':
+            if line['language'] == 'English':
+                question += "\nPlease answer this question with reasoning. First output your reasoning process in <think> </think> tags and then output the final answer in <answer> </answer> tags."  # noqa: E501
+            else:
+                question += "\n请用推理来回答这个问题。首先在<think></think>标签中输出推理过程，然后在<answer></answer>标签中输入最终答案。"  # noqa: E501
+        else:
+            if line['language'] == 'English':
+                question += "\nAnswer this question directly with numbers, formulas, or phrases"
+            else:
+                question += "\n请直接用数字、公式或短语回答这个问题。"
+        try:
+            if line['sig_figs']:
+                sf = str(int(line['sig_figs']))
+                if line['language'] == 'English':
+                    question += f"The final answer should retain {sf} significant figures."
+                else:
+                    question += f"最终答案应保留{sf}位有效数字。"
+        except Exception:
+            pass
+        msgs = []
+        if os.environ.get('USE_IMAGE', '1') == '1':
+            if isinstance(tgt_path, list):
+                msgs.extend([dict(type='image', value=p) for p in tgt_path])
+            else:
+                msgs = [dict(type='image', value=tgt_path)]
+        msgs.append(dict(type='text', value=question))
+        return msgs
+
+    @classmethod
+    def evaluate(self, eval_file, **judge_kwargs):
+        from .utils.seephys import extract, eval_acc
+
+        model = judge_kwargs.pop('model', 'deepseek')
+        suffix = eval_file.split('.')[-1]
+        storage = eval_file.replace(f'.{suffix}', f'_{model}.xlsx')
+        tmp_file = eval_file.replace(f'.{suffix}', f'_{model}.pkl')
+        nproc = judge_kwargs.pop('nproc', 4)
+        if not osp.exists(storage):
+            data = load(eval_file)
+            model = build_judge(model=model, max_tokens=1024, **judge_kwargs)
+            assert model.working(), ('SeePhys evaluation requires a working OPENAI API\n' + DEBUG_MESSAGE)
+            lt = len(data)
+            lines = [data.iloc[i] for i in range(lt)]
+            tups = [(model, line) for line in lines]
+            indices = [line['index'] for line in lines]
+            ans = {}
+            if osp.exists(tmp_file):
+                ans = load(tmp_file)
+            tups = [x for x, i in zip(tups, indices) if i not in ans]
+            indices = [i for i in indices if i not in ans]
+
+            if len(indices):
+                new_results = track_progress_rich(
+                    extract,
+                    tups,
+                    nproc=nproc,
+                    chunksize=nproc,
+                    keys=indices,
+                    save=tmp_file,
+                )
+                ans = load(tmp_file)
+                for k, v in zip(indices, new_results):
+                    assert k in ans
+                    assert ans[k]['log'] == v['log'] and ans[k]['extract'] == v['extract'] and ans[k]['score'] == v[
+                        'score']
+
+            data['extract'] = [ans[idx]['extract'] for idx in data['index']]
+            data['log'] = [ans[idx]['log'] for idx in data['index']]
+            data['score'] = [ans[idx]['score'] for idx in data['index']]
+
+            dump(data, storage)
+
+        score = eval_acc(storage)
+        score_pth = storage.replace('.xlsx', '_score.json')
+        dump(score, score_pth)
+        return score
+
+
 class LogicVista(ImageBaseDataset):
     TYPE = 'VQA'
     DATASET_URL = {
