fa37dfe8c57404a7291ebeeebcd970243708853b
Ezra-Yu <18586273+Ezra-Yu@users.noreply.github.co>
2025-07-20 21:04:58 +0800

    update

--- vlmeval/config.py
+++ vlmeval/config.py
@@ -496,6 +496,9 @@ api_models = {
     "GeminiPro2-5": partial(
         Gemini, model="gemini-2.5-pro", temperature=0, retry=10
     ),
+    "GeminiPro2-5-32k": partial(
+        GPT4V, model="gemini-2.5-pro", temperature=0, retry=5, timeout=180, max_tokens=32768
+    ),
     
     # Qwen-VL
     "QwenVLPlus": partial(QwenVLAPI, model="qwen-vl-plus", temperature=0, retry=10),
