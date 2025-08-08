80b3bcd8767ba51f819c4f0e446559fab5701aba
Ezra-Yu <1105212286@qq.com>
2025-08-08 19:09:54 +0800

    Update jt_vl_chat.py

--- vlmeval/api/jt_vl_chat.py
+++ vlmeval/api/jt_vl_chat.py
@@ -175,14 +175,8 @@ class JTVLChatWrapper(BaseAPI):
             "image_base64": image,
             "max_tokens": max_tokens,
             "temperature": temperature,
-<<<<<<< HEAD
-            "do_sample": False,
-            "understanding_plus":understanding_plus,
-            "stream": stream
-=======
             "do_sample":False,
             "understanding_plus":False
->>>>>>> 97489e1 ([API] change app_code and app_endpoint (#1179))
         }
         return send_data
 
@@ -196,13 +190,8 @@ class JTVLChatWrapper(BaseAPI):
             ],
             "max_tokens": max_tokens,
             "temperature": temperature,
-<<<<<<< HEAD
             "stream": stream,
             "understanding_plus":understanding_plus
-=======
-            "do_sample":False,
-            "understanding_plus":False
->>>>>>> 97489e1 ([API] change app_code and app_endpoint (#1179))
         }
         return send_data
 
