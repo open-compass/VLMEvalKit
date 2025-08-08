3149f58446562c0025dc0ecfc6e90093361ce383
Ezra-Yu <1105212286@qq.com>
2025-08-08 19:16:47 +0800

    Update __init__.py

--- vlmeval/vlm/__init__.py
+++ vlmeval/vlm/__init__.py
@@ -103,3 +103,8 @@ from .flash_vl import FlashVL
 from .oryx import Oryx
 from .treevgr import TreeVGR
 from .glm4_1v import GLM4_1v
+from .varco_vision import VarcoVision
+from .qtunevl import (
+    QTuneVL,
+    QTuneVLChat,
+)
