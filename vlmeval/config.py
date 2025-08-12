4f590135be4aaac938f322c107cc8bfa879a5364
kennymckormick <dhd.efz@gmail.com>
2025-08-12 21:05:37 +0800

    Fix SAIL-VL 1.7 pth

--- vlmeval/config.py
+++ vlmeval/config.py
@@ -962,8 +962,8 @@ sail_series = {
     "SAIL-VL-1.5-2B": partial(SailVL, model_path="BytedanceDouyinContent/SAIL-VL-1d5-2B", use_msac = True),
     "SAIL-VL-1.5-8B": partial(SailVL, model_path="BytedanceDouyinContent/SAIL-VL-1d5-8B", use_msac = True),
     "SAIL-VL-1.6-8B": partial(SailVL, model_path="BytedanceDouyinContent/SAIL-VL-1d6-8B", use_msac = True),
-    "SAIL-VL-1.6-Thinking-2B-2507": partial(SailVL, model_path="BytedanceDouyinContent/SAIL-VL-1.6-Thinking-2B-2507", use_msac = True, use_cot=True),
-    "SAIL-VL-1.6-Thinking-8B-2507": partial(SailVL, model_path="BytedanceDouyinContent/SAIL-VL-1.6-Thinking-8B-2507", use_msac = True, use_cot=True),
+    "SAIL-VL-1.7-Thinking-2B-2507": partial(SailVL, model_path="BytedanceDouyinContent/SAIL-VL-1d7-Thinking-2B-2507", use_msac = True, use_cot=True),
+    "SAIL-VL-1.7-Thinking-8B-2507": partial(SailVL, model_path="BytedanceDouyinContent/SAIL-VL-1d7-Thinking-8B-2507", use_msac = True, use_cot=True),
 }
 
 ristretto_series = {
