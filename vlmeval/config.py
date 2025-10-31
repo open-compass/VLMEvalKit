03a37fcacab6603b151dafecd5f19d1af8cc1346
Lei Gao <122810012+Graysonicc@users.noreply.github.com>
2025-10-31 20:55:08 +0800

    [Model] add rbdashmm 3 and 3.5-38b (#1295)
    * v1024
    
    * v1027
    
    * v1027_3
    
    * v1029_1
    
    * 1029_test
    
    * 3_5
    
    * reasoning_38b change cut
    
    ---------
    
    Co-authored-by: Xinyu Fang <fangxinyutju202009@126.com>

--- vlmeval/config.py
+++ vlmeval/config.py
@@ -1727,6 +1727,24 @@ qtunevl_series = {
     ),
 }
 
+# RbdashMM series via lmdeploy API
+rbdashmm_api_series_lmdeploy = {
+    "rbdashmm3_DPO_38B_api": partial(
+        RBdashMMChat3_API,
+        api_base="http://0.0.0.0:23333/v1/chat/completions",
+        temperature=0,
+        retry=3,
+        timeout=600
+    ),
+    "rbdashmm3_5_DPO_38B_api": partial(
+        RBdashChat3_5_API,
+        api_base="http://0.0.0.0:23333/v1/chat/completions",
+        temperature=0,
+        retry=3,
+        timeout=600
+    )
+}
+
 logics_series = {
     "Logics-Thinking-8B": partial(Logics_Thinking,model_path='Logics-MLLM/Logics-Thinking-8B'),
     "Logics-Thinking-32B": partial(Logics_Thinking,model_path='Logics-MLLM/Logics-Thinking-32B'),
@@ -1776,9 +1794,9 @@ model_groups = [
     kosmos_series, points_series, nvlm_series, vintern_series, h2ovl_series,
     aria_series, smolvlm_series, sail_series, valley_series, vita_series,
     ross_series, emu_series, ola_series, ursa_series, gemma_series,
-    long_vita_series, ristretto_series, kimi_series, aguvis_series, hawkvl_series, 
-    flash_vl, kimi_vllm_series, oryx_series, treevgr_series, varco_vision_series, qtunevl_series, xvl_series, thyme_series, logics_series, 
-    cosmos_series, keye_series, qianfanvl_series, lfm2vl_series
+    long_vita_series, ristretto_series, kimi_series, aguvis_series, hawkvl_series,
+    flash_vl, kimi_vllm_series, oryx_series, treevgr_series, varco_vision_series, qtunevl_series, xvl_series, thyme_series, logics_series,
+    cosmos_series, keye_series, qianfanvl_series, lfm2vl_series, rbdashmm_api_series_lmdeploy
 ]
 
 for grp in model_groups:
