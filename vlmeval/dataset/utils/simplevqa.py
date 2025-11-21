f0d1ea3aea5f3a6dce13653eab0bdc68a843e341
Bujiazi <3351506128@qq.com>
2025-11-21 12:32:26 +0800

    fix flake8

--- vlmeval/dataset/utils/simplevqa.py
+++ vlmeval/dataset/utils/simplevqa.py
@@ -1,13 +1,13 @@
-import random 
+import random
 import re
 import pandas
-
 import json
 import copy
 import pdb
 
 model_keys = ["model_response"]
 mapper = {"正确": "is_correct", "错误": "is_incorrect", "未尝试": "is_not_attempted"}
+
 
 def divide_dict_values(d, divisor):
     for key, value in d.items():
@@ -21,29 +21,29 @@ def divide_dict_values(d, divisor):
 def SimpleVQAEval(data):
     amount = len(data)
     result_metrics = {}
-    
+
     for idx, line in enumerate(data):
         judge_res = line["judge_res"].copy()
-        for key, value in judge_res.items(): 
+        for key, value in judge_res.items():
             if isinstance(value, dict):
                 judge_res[key] = value["conclusion"].replace("**", "")
             elif judge_res[key] not in ["正确", "错误", "未尝试"]:
                 judge_res[key] = judge_res[key].replace("**", "")
                 if len(judge_res[key]) > 3:
                     judge_res[key] = "未尝试"
-                
+
             if key not in result_metrics:
                 result_metrics[key] = {
                     "is_correct": 0,
                     "is_incorrect": 0,
                     "is_not_attempted": 0
                 }
-            
+
             try:
                 result_metrics[key][mapper[judge_res[key]]] += 1
-            except Exception as e:
+            except Exception:
                 print(f"Error in Mapper Key: {judge_res[key]}")
-            
+
             if key not in model_keys:
                 model_keys.append(key)
 
@@ -57,7 +57,8 @@ def SimpleVQAEval(data):
             "is_incorrect": round(result_metrics[model_key]["is_incorrect"] / amount, 4),
             "is_not_attempted": round(result_metrics[model_key]["is_not_attempted"] / amount, 4),
         }
-        aggregate_metrics["is_given_attempted"] = round(aggregate_metrics["is_correct"] + aggregate_metrics["is_incorrect"], 4)
+        aggregate_metrics["is_given_attempted"] = round(
+            aggregate_metrics["is_correct"] + aggregate_metrics["is_incorrect"], 4)
         # Calculate accuracy_given_attempted
         aggregate_metrics["accuracy_given_attempted"] = (
             aggregate_metrics["is_correct"]
@@ -66,24 +67,23 @@ def SimpleVQAEval(data):
             else 0
         )
         aggregate_metrics["f1"] = (
-                2 * aggregate_metrics["accuracy_given_attempted"] * aggregate_metrics["is_correct"]
-                / (aggregate_metrics["accuracy_given_attempted"] + aggregate_metrics["is_correct"])
-                if (aggregate_metrics["accuracy_given_attempted"] + aggregate_metrics["is_correct"]) > 0
-                else 0
-            )
-        print(model_key + ": AGGREGATE METRICS") 
+            2 * aggregate_metrics["accuracy_given_attempted"] * aggregate_metrics["is_correct"]
+            / (aggregate_metrics["accuracy_given_attempted"] + aggregate_metrics["is_correct"])
+            if (aggregate_metrics["accuracy_given_attempted"] + aggregate_metrics["is_correct"]) > 0
+            else 0
+        )
+        print(model_key + ": AGGREGATE METRICS")
         print(aggregate_metrics)
         print("##################")
-        
+
         res_json.append(aggregate_metrics)
-        
+
         output_d = {
             "accuracy_given_attempted": aggregate_metrics["accuracy_given_attempted"],
             "f1": aggregate_metrics["f1"]
         }
-        
+
         print(f"Accuracy Given Attempted: {output_d['accuracy_given_attempted']:.3f}")
         print(f"F1 Score: {output_d['f1']:.3f}")
-        
+
     return aggregate_metrics
-    
