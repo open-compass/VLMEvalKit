cdd0060405d9cab99fd528567abd2277044af26b
Hangui Lin <linhangui@uir.edu.cn>
2025-12-28 10:22:06 +0800

    Add TextHaluBench benchmark for scene text hallucination evaluation

--- vlmeval/dataset/__init__.py
+++ vlmeval/dataset/__init__.py
@@ -78,7 +78,7 @@ from .GUI.screenspot_pro import ScreenSpot_Pro
 from .mmifeval import MMIFEval
 from .chartmimic import ChartMimic
 from .m4bench import M4Bench
-
+from .texthalubench import TextHaluBench
 
 class ConcatDataset(ImageBaseDataset):
     # This dataset takes multiple dataset names as input and aggregate them into a single dataset.
@@ -205,7 +205,7 @@ IMAGE_DATASET = [
     ZEROBench, SCAM, Omni3DBench, TallyQA, _3DSRBench, BMMR, AffordanceDataset,
     MMEReasoning, GOBenchDataset, SFE, ChartMimic, MMVMBench, XLRSBench,
     OmniEarthMCQBench, VisFactor, OSTDataset, OCRBench_v2, TreeBench, CVQA, M4Bench,
-    AyaVisionBench, TopViewRS, VLMBias
+    AyaVisionBench, TopViewRS, VLMBias, TextHaluBench
 ]
 
 VIDEO_DATASET = [
