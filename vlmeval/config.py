c4dddc547e3d6024db41d902ccd1599493727b18
Wakals <2200017821@stu.pku.edu.cn>
2026-01-03 15:07:03 +0000

    Support CoVT series

--- vlmeval/config.py
+++ vlmeval/config.py
@@ -1981,6 +1981,37 @@ lfm2vl_series = {
     "LFM2-VL-3B": partial(LFM2VL, model_path="LiquidAI/LFM2-VL-3B"),
 }
 
+covt_series = {
+    "CoVT-7B-seg": partial(
+        CoVTChat,
+        model_path="Wakals/CoVT-7B-seg",
+        min_pixels=1280 * 28 * 28,
+        max_pixels=16384 * 28 * 28,
+        use_custom_prompt=False,
+    ),
+    "CoVT-7B-depth": partial(
+        CoVTChat,
+        model_path="Wakals/CoVT-7B-depth",
+        min_pixels=1280 * 28 * 28,
+        max_pixels=16384 * 28 * 28,
+        use_custom_prompt=False,
+    ),
+    "CoVT-7B-seg_depth_dino": partial(
+        CoVTChat,
+        model_path="Wakals/CoVT-7B-seg_depth_dino",
+        min_pixels=1280 * 28 * 28,
+        max_pixels=16384 * 28 * 28,
+        use_custom_prompt=False,
+    ),
+    "CoVT-7B-seg_depth_dino_edge": partial(
+        CoVTChat,
+        model_path="Wakals/CoVT-7B-seg_depth_dino_edge",
+        min_pixels=1280 * 28 * 28,
+        max_pixels=16384 * 28 * 28,
+        use_custom_prompt=False,
+    ),
+}
+
 internvl_groups = [
     internvl, internvl2, internvl2_5, mini_internvl, internvl2_5_mpo, 
     internvl3, internvl3_5
@@ -2012,7 +2043,7 @@ model_groups = [
     long_vita_series, ristretto_series, kimi_series, aguvis_series, hawkvl_series,
     flash_vl, kimi_vllm_series, oryx_series, treevgr_series, varco_vision_series, qtunevl_series, 
     xvl_series, thyme_series, logics_series, cosmos_series, keye_series, qianfanvl_series, 
-    lfm2vl_series, rbdashmm_api_series_lmdeploy, interns1_series, insight_v_series
+    lfm2vl_series, rbdashmm_api_series_lmdeploy, interns1_series, insight_v_series, covt_series
 ]
 
 for grp in model_groups:
