d09317975901e7429a14a6868e2b783070fc5c15
Peter_Stacy <90747373+PeterWangyi@users.noreply.github.com>
2026-01-09 19:01:02 +0800

    [Model] Add SenseNova-SI latest models (#38)
    * [Feature] Init commit for STI-Bench
    
    * [Feature] add sensenova-si latest models
    
    * [Feature] update stibench tsv hf download path
    
    * [Refactor] upgrade acccording to copilot

--- vlmeval/config.py
+++ vlmeval/config.py
@@ -2095,7 +2095,43 @@ sensenova_si_series = {
         use_custom_prompt=False,
         version="V2.0"
     ),
-    
+    "SenseNova-SI-1.1-Qwen2.5-VL-3B": partial(
+        Qwen2VLChat, 
+        model_path="sensenova/SenseNova-SI-1.1-Qwen2.5-VL-3B", 
+        min_pixels=1280 * 28 * 28,
+        max_pixels=16384 * 28 * 28,
+        use_custom_prompt=False,
+    ),
+    "SenseNova-SI-1.1-Qwen2.5-VL-7B": partial(
+        Qwen2VLChat, 
+        model_path="sensenova/SenseNova-SI-1.1-Qwen2.5-VL-7B", 
+        min_pixels=1280 * 28 * 28,
+        max_pixels=16384 * 28 * 28,
+        use_custom_prompt=False,
+    ),
+    "SenseNova-SI-1.1-Qwen3-VL-8B": partial(
+        Qwen3VLChat,
+        model_path="sensenova/SenseNova-SI-1.1-Qwen3-VL-8B",
+        use_custom_prompt=False,
+    ),
+    "SenseNova-SI-1.1-BAGEL-7B-MoT": partial(
+        Bagel, 
+        model_path='sensenova/SenseNova-SI-1.1-BAGEL-7B-MoT'
+    ),
+    # SenseNova-SI-1.2 series
+    "SenseNova-SI-1.2-InternVL3-8B": partial(
+        InternVLChat, 
+        model_path="sensenova/SenseNova-SI-1.2-InternVL3-8B", 
+        use_custom_prompt=False,
+        version="V2.0"
+    ),
+    # SenseNova-SI-1.3 series
+    "SenseNova-SI-1.3-InternVL3-8B": partial(
+        InternVLChat, 
+        model_path="sensenova/SenseNova-SI-1.3-InternVL3-8B", 
+        use_custom_prompt=False,
+        version="V2.0"
+    ),
 }
 
 internvl_groups = [
