a9bc163b3979730ff14ab3e92374fbdb44eae1af
Li-Jinsong <lijinsong0130@gmail.com>
2026-02-06 15:03:51 +0000

    support plotqa

--- vlmeval/dataset/__init__.py
+++ vlmeval/dataset/__init__.py
@@ -35,6 +35,7 @@ from .chartmuseum import ChartMuseum
 from .chartqapro import ChartQAPro
 from .refcoco import RefCOCODataset
 from .simplevqa import SimpleVQA
+from .plotqa import PlotQA
 
 from .mmbench_video import MMBenchVideo
 from .videomme import VideoMME
@@ -232,7 +233,7 @@ IMAGE_DATASET = [
     AyaVisionBench, TopViewRS, VLMBias, MMHELIX, MedqbenchMCQDataset, MathCanvas, MMReason,
     MedqbenchPairedDescriptionDataset, MedqbenchCaptionDataset, ChartMuseum, ChartQAPro, ReasonMap_Plus,
     olmOCRBench, OceanOCRBench, MATBench, VLRMBench, RefCOCODataset, SimpleVQA, HiPhODataset, MaCBench,
-    UniSVG, SArena_MINI, MMSIVideoBench, VLMsAreBiased
+    UniSVG, SArena_MINI, MMSIVideoBench, PlotQA, VLMsAreBiased
 ]
 
 VIDEO_DATASET = [
