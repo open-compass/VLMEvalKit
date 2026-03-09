86e37c6fa339249630da12bf22ae4e5e8bf55265
TianhaoLiang2000 <2662248501@qq.com>
2026-03-09 11:16:30 +0800

    [Fix] Comment out missing module imports inherited from upstream/run-api
    telemm_thinking, together, gcp_vertex, bedrock do not exist in this
    branch; comment them out to unblock import.

--- vlmeval/api/__init__.py
+++ vlmeval/api/__init__.py
@@ -10,7 +10,7 @@ from .cloudwalk import CWWrapper
 from .sensechat_vision import SenseChatVisionAPI
 from .siliconflow import SiliconFlowAPI, TeleMMAPI
 from .telemm import TeleMM2_API
-from .telemm_thinking import TeleMM2Thinking_API
+# from .telemm_thinking import TeleMM2Thinking_API  # module not found in upstream/run-api
 from .hunyuan import HunyuanVision
 from .bailingmm import bailingMMAPI
 from .bluelm_api import BlueLMWrapper, BlueLM_API
@@ -27,9 +27,9 @@ from .mug_u import MUGUAPI
 from .kimivl_api import KimiVLAPIWrapper, KimiVLAPI
 from .rbdashmm_chat3_api import RBdashMMChat3_API, RBdashChat3_5_API
 from .rbdashmm_chat3_5_api import RBdashMMChat3_78B_API, RBdashMMChat3_5_38B_API
-from .together import TogetherAPI
-from .gcp_vertex import GCPVertexAPI
-from .bedrock import BedrockAPI
+# from .together import TogetherAPI         # not available in this branch
+# from .gcp_vertex import GCPVertexAPI      # not available in this branch
+# from .bedrock import BedrockAPI           # not available in this branch
 
 __all__ = [
     'OpenAIWrapper', 'HFChatModel', 'GeminiWrapper', 'GPT4V', 'Gemini',
@@ -40,6 +40,5 @@ __all__ = [
     'OpenAISDKWrapper', 'LMDeployWrapper',
     'TaichuVLAPI', 'TaichuVLRAPI', 'DoubaoVL', "MUGUAPI", 'KimiVLAPIWrapper', 'KimiVLAPI',
     'RBdashMMChat3_API', 'RBdashChat3_5_API', 'RBdashMMChat3_78B_API', 'RBdashMMChat3_5_38B_API',
-    'VideoChatOnlineV2API', 'TeleMM2_API', 'TeleMM2Thinking_API',
-    'TogetherAPI', 'GCPVertexAPI', 'BedrockAPI',
+    'VideoChatOnlineV2API', 'TeleMM2_API',
 ]
