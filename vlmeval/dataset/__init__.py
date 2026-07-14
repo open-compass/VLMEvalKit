a8b12bf1c3737a33fc1de967c202f9c592b22e86
ADAM HUANG <68174921+shinmohuang@users.noreply.github.com>
2026-07-14 07:54:55 +0100

    [Benchmark] Add Spatial-DISE benchmark (#1542)
    * Add Spatial-DISE benchmark
    
    * Use merged and separate Spatial-DISE images
    
    * Split Spatial-DISE image input modes
    
    * Support dynamic Spatial-DISE answer options

--- vlmeval/dataset/__init__.py
+++ vlmeval/dataset/__init__.py
@@ -131,6 +131,7 @@ from .siuo_mcq import SIUOMCQDataset
 from .slidevqa import SlideVQA
 from .sparbench import SparBench
 from .spatial457 import Spatial457
+from .spatial_dise import SpatialDISE
 from .spatialvizbench import SpatialVizBench
 from .spbench import SPBench
 from .ssi_bench import SSIBenchDataset
@@ -310,7 +311,7 @@ IMAGE_DATASET = [
 # add by EASI team
 IMAGE_DATASET += [
     MindCubeBench, EmbSpatialBench, ViewSpatialBench, MMSIBench, SiteBenchImage,
-    SparBench, SpatialVizBench, StareBench, OmniSpatialBench, ERQABench, RoboSpatialBench, RefSpatialBench,  # noqa: E501
+    SparBench, SpatialVizBench, SpatialDISE, StareBench, OmniSpatialBench, ERQABench, RoboSpatialBench, RefSpatialBench,  # noqa: E501
     SPBench, ERIQBench, DA2K
 ]
 
