2b3f32d51b1a612f8758777bdbf360328a74a94b
ssiq <liweiwuhome@hotmail.com>
2026-08-07 16:53:47 +0800

    Add SUPERChem dataset (#1623)

--- vlmeval/dataset/__init__.py
+++ vlmeval/dataset/__init__.py
@@ -139,6 +139,7 @@ from .spbench import SPBench
 from .ssi_bench import SSIBenchDataset
 from .starebench import StareBench
 from .stibench import STIBench
+from .superchem import SUPERChemDataset
 from .tamperbench import MVTamperBench
 from .tempcompass import TempCompass, TempCompass_Captioning, TempCompass_MCQ, TempCompass_YorN
 from .text_mcq import CustomTextMCQDataset, TextMCQDataset
@@ -309,7 +310,7 @@ IMAGE_DATASET = [
     SciDocBench, OmniMat,
     MMRarebenchDiagnosis, MMRarebenchTreatment, MMRarebenchCrossmodal, MMRarebenchExamination,
     MRareBenchDiagnosis, MRareBenchEvidenceVerif,
-    BabyVision,
+    BabyVision, SUPERChemDataset,
 ]
 
 # add by EASI team
