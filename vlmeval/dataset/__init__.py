1fffac0bfc57dff6b42ba3d8217c3b70a094313e
liuzhipenggg <liuzp@ihep.ac.cn>
2026-08-14 22:54:06 +0800

    [Benchmark] Add support for CAPEval

--- vlmeval/dataset/__init__.py
+++ vlmeval/dataset/__init__.py
@@ -9,6 +9,7 @@ from vlmeval.smp import LMUDataRoot, dump, get_intermediate_file_path, load, loc
 from .asclepius import Asclepius
 from .av_speakerbench import AVSpeakerBench
 from .babyvision import BabyVision
+from .capeval import CAPEval
 from .CGAVCounting.cg_av_counting import CGAVCounting
 from .cgbench import (CGBench_MCQ_Grounding, CGBench_MCQ_Grounding_Mini, CGBench_OpenEnded,
                       CGBench_OpenEnded_Mini)
@@ -311,7 +312,7 @@ IMAGE_DATASET = [
     SciDocBench, OmniMat,
     MMRarebenchDiagnosis, MMRarebenchTreatment, MMRarebenchCrossmodal, MMRarebenchExamination,
     MRareBenchDiagnosis, MRareBenchEvidenceVerif,
-    BabyVision, PerceptionBench, SUPERChemDataset,
+    BabyVision, PerceptionBench, SUPERChemDataset, CAPEval,
 ]
 
 # add by EASI team
